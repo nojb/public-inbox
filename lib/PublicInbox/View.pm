nojb/public-inbox

commit fefea3d7d2484ffbf433aec0dd80026aa7120e07

ensure per-message short quotes do not get too long
Sometimes a single long word or URL can lengthen the line
too much.

lib/PublicInbox/View.pm
@@ -9,6 +9,7 @@ use Encode qw/find_encoding/;
 use Encode::MIME::Header;
 use Email::MIME::ContentType qw/parse_content_type/;
 use constant MAX_INLINE_QUOTED => 5;
+use constant MAX_TRUNC_LEN => 72;
 
 my $enc_utf8 = find_encoding('utf8');
 my $enc_ascii = find_encoding('us-ascii');
@@ -91,18 +92,24 @@ sub add_text_body_short {
 	my $s = ascii_html($enc->decode($part->body));
 	$s =~ s!^((?:(?:&gt;[^\n]*)\n)+)!
 		my $cur = $1;
-		my @lines = split(/\n/, $cur);
+		my @lines = split(/\n(?:&gt;\s*)?/, $cur);
 		if (@lines > MAX_INLINE_QUOTED) {
 			# show a short snippet of quoted text
 			$cur = join(' ', @lines);
-			$cur =~ s/&gt; ?//g;
+			$cur =~ s/^&gt;\s*//;
 
 			my @sum = split(/\s+/, $cur);
 			$cur = '';
 			do {
-				$cur .= shift(@sum) . ' ';
-			} while (@sum && length($cur) < 64);
-			$cur=~ s/ \z/ .../;
+				my $tmp = shift(@sum);
+				my $len = length($tmp) + length($cur);
+				if ($len > MAX_TRUNC_LEN) {
+					@sum = ();
+				} else {
+					$cur .= $tmp . ' ';
+				}
+			} while (@sum && length($cur) < MAX_TRUNC_LEN);
+			$cur =~ s/ \z/ .../;
 			"&gt; &lt;<a href=\"${full_pfx}#q${part_nr}_" . $n++ .
 				"\">$cur<\/a>&gt;\n";
 		} else {

commit 24edcaa76e03ddfba58bf0074d89e693de6eddb4

view: fix regression in standalone /^&gt;$/ lines
The lack of trailing whitespace in quote prefixes threw us
off and cause t/view to fail.

This failure was caused by
commit fefea3d7d2484ffbf433aec0dd80026aa7120e07
("ensure per-message short quotes do not get too long")
and not caught before pushing because I failed to run
"make", only "prove" (and not even "prove -l" :x).

lib/PublicInbox/View.pm
@@ -92,7 +92,7 @@ sub add_text_body_short {
 	my $s = ascii_html($enc->decode($part->body));
 	$s =~ s!^((?:(?:&gt;[^\n]*)\n)+)!
 		my $cur = $1;
-		my @lines = split(/\n(?:&gt;\s*)?/, $cur);
+		my @lines = split(/\n/, $cur);
 		if (@lines > MAX_INLINE_QUOTED) {
 			# show a short snippet of quoted text
 			$cur = join(' ', @lines);