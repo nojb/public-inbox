nojb/public-inbox

commit 72c0f7c71ff28de9755dc4aee8b6ce6f0e4f2ed7

feed: merge subjects regardless of "[PATCH vN]"
This normalizes rerolled patches with identical topics,
but does not normalize different patches even if they are
in the same thread (for now).

lib/PublicInbox/Feed.pm
@@ -332,8 +332,12 @@ sub add_topic {
 		$subj = mime_header($header_obj, 'Subject');
 	}
 
-	$subj = $srch->subject_normalized($subj);
-	if (++$subjs->{$subj} == 1) {
+	my $topic = $subj = $srch->subject_normalized($subj);
+
+	# kill "[PATCH v2]" etc. for summarization
+	$topic =~ s/\A\s*\[[^\]]+\]\s*//g;
+
+	if (++$subjs->{$topic} == 1) {
 		unless ($header_obj) {
 			my $mime = do_cat_mail($git, $path) or return 0;
 			$header_obj = $mime->header_obj;

commit f85f1781634dc03e093a0790a41bb4ae90ea907b

feed: fix "created" vs "updated" diff in topics
This fixes a regression introduced in
commit 72c0f7c71ff28de9755dc4aee8b6ce6f0e4f2ed7
(feed: merge subjects regardless of "[PATCH vN]")

lib/PublicInbox/Feed.pm
@@ -345,7 +345,7 @@ sub add_topic {
 		my $mid = $header_obj->header('Message-ID');
 		$mid = mid_compress(mid_clean($mid));
 		$u = $enc_utf8->decode($u);
-		push @$order, [ $mid, $ts, $u, $subj ];
+		push @$order, [ $mid, $ts, $u, $subj, $topic ];
 		return 1;
 	}
 	0; # old topic, continue going
@@ -357,8 +357,8 @@ sub dump_topics {
 	my $dst = '';
 	$dst .= "\n[No recent topics]" unless (scalar @$order);
 	while (defined(my $info = shift @$order)) {
-		my ($mid, $ts, $u, $subj) = @$info;
-		my $n = delete $subjs->{$subj};
+		my ($mid, $ts, $u, $subj, $topic) = @$info;
+		my $n = delete $subjs->{$topic};
 		$mid = PublicInbox::Hval->new($mid)->as_href;
 		$subj = PublicInbox::Hval->new($subj)->as_html;
 		$u = PublicInbox::Hval->new($u)->as_html;