nojb/public-inbox

commit 81a9c1b476987d845b340ab9013d26cf4487cb9a

search: disable Message-ID compression in Xapian
We'll continue to compress long Message-IDs in URLs (which we know
about), but we will store entire Message-IDs in the Xapian database
to facilitate ease-of-lookups in external databases.

lib/PublicInbox/ExtMsg.pm
@@ -12,7 +12,6 @@ sub ext_msg {
 	my $pi_config = $ctx->{pi_config};
 	my $listname = $ctx->{listname};
 	my $mid = $ctx->{mid};
-	my $cmid = mid_compress($mid);
 
 	eval { require PublicInbox::Search };
 	my $have_xap = $@ ? 0 : 1;
@@ -35,13 +34,13 @@ sub ext_msg {
 		if ($have_xap) {
 			my $doc_id = eval {
 				my $s = PublicInbox::Search->new($git_dir);
-				$s->find_unique_doc_id('mid', $cmid);
+				$s->find_unique_doc_id('mid', $mid);
 			};
 			if ($@) {
 				# xapian not configured for this repo
 			} else {
 				# maybe we found it!
-				return r302($url, $cmid) if (defined $doc_id);
+				return r302($url, $mid) if (defined $doc_id);
 
 				# no point in trying the fork fallback if we
 				# know Xapian is up-to-date but missing the
@@ -55,7 +54,7 @@ sub ext_msg {
 	}
 
 	# Xapian not installed or configured for some repos
-	my $path = "HEAD:" . mid2path($cmid);
+	my $path = "HEAD:" . mid2path($mid);
 
 	foreach my $n (@nox) {
 		my @cmd = ('git', "--git-dir=$n->{git_dir}", 'cat-file',
@@ -70,7 +69,7 @@ sub ext_msg {
 			my $type = eval { local $/; <$fh> };
 			close $fh;
 			if ($? == 0 && $type eq "blob\n") {
-				return r302($n->{url}, $cmid);
+				return r302($n->{url}, $mid);
 			}
 		}
 	}

lib/PublicInbox/Search.pm
@@ -26,14 +26,15 @@ use constant {
 	# 6 - preserve References: order in document data
 	# 7 - remove references and inreplyto terms
 	# 8 - remove redundant/unneeded document data
-	SCHEMA_VERSION => 8,
+	# 9 - disable Message-ID compression
+	SCHEMA_VERSION => 9,
 	QP_FLAGS => FLAG_PHRASE|FLAG_BOOLEAN|FLAG_LOVEHATE|FLAG_WILDCARD,
 };
 
 # setup prefixes
 my %bool_pfx_internal = (
 	type => 'T', # "mail" or "ghost"
-	mid => 'Q', # uniQue id (Message-ID or mid_compress)
+	mid => 'Q', # uniQue id (Message-ID)
 );
 
 my %bool_pfx_external = (
@@ -171,7 +172,6 @@ sub date_range_processor {
 sub lookup_message {
 	my ($self, $mid) = @_;
 	$mid = mid_clean($mid);
-	$mid = mid_compress($mid);
 
 	my $doc_id = $self->find_unique_doc_id('mid', $mid);
 	my $smsg;

lib/PublicInbox/SearchIdx.pm
@@ -41,8 +41,7 @@ sub add_message {
 	my $db = $self->{xdb};
 
 	my $doc_id;
-	my $mid_orig = mid_clean($mime->header('Message-ID'));
-	my $mid = mid_compress($mid_orig);
+	my $mid = mid_clean($mime->header('Message-ID'));
 	my $was_ghost = 0;
 	my $ct_msg = $mime->header('Content-Type') || 'text/plain';
 
@@ -139,7 +138,7 @@ sub add_message {
 	};
 
 	if ($@) {
-		warn "failed to index message <$mid_orig>: $@\n";
+		warn "failed to index message <$mid>: $@\n";
 		return undef;
 	}
 	$doc_id;
@@ -147,11 +146,10 @@ sub add_message {
 
 # returns deleted doc_id on success, undef on missing
 sub remove_message {
-	my ($self, $mid_orig) = @_;
+	my ($self, $mid) = @_;
 	my $db = $self->{xdb};
 	my $doc_id;
-	$mid_orig = mid_clean($mid_orig);
-	my $mid = mid_compress($mid_orig);
+	$mid = mid_clean($mid);
 
 	eval {
 		$doc_id = $self->find_unique_doc_id('mid', $mid);
@@ -159,7 +157,7 @@ sub remove_message {
 	};
 
 	if ($@) {
-		warn "failed to remove message <$mid_orig>: $@\n";
+		warn "failed to remove message <$mid>: $@\n";
 		return undef;
 	}
 	$doc_id;
@@ -204,32 +202,24 @@ sub link_message {
 sub link_message_to_parents {
 	my ($self, $smsg) = @_;
 	my $doc = $smsg->{doc};
-	my $mid = mid_compress($smsg->mid);
+	my $mid = $smsg->mid;
 	my $mime = $smsg->mime;
 	my $refs = $mime->header('References');
 	my @refs = $refs ? ($refs =~ /<([^>]+)>/g) : ();
-	my $irt = $mime->header('In-Reply-To');
-	if ($irt) {
-		$irt = mid_compress(mid_clean($irt));
-
-		# maybe some crazies will try to make a circular reference:
-		if ($irt eq $mid) {
-			$irt = undef;
-		} else {
-			# last References should be $irt
-			# we will de-dupe later
-			push @refs, $irt;
-		}
+	if (my $irt = $mime->header('In-Reply-To')) {
+		# last References should be $irt
+		# we will de-dupe later
+		push @refs, mid_clean($irt);
 	}
 
 	my $tid;
 	if (@refs) {
-		my @crefs = map { mid_compress($_) } @refs;
 		my %uniq = ($mid => 1);
+		my @orig_refs = @refs;
+		@refs = ();
 
 		# prevent circular references via References: here:
-		@refs = ();
-		foreach my $ref (@crefs) {
+		foreach my $ref (@orig_refs) {
 			next if $uniq{$ref};
 			$uniq{$ref} = 1;
 			push @refs, $ref;
@@ -342,7 +332,6 @@ sub _resolve_mid_to_tid {
 sub create_ghost {
 	my ($self, $mid, $tid) = @_;
 
-	$mid = mid_compress($mid);
 	$tid = $self->next_thread_id unless defined $tid;
 
 	my $doc = Search::Xapian::Document->new;

lib/PublicInbox/SearchMsg.pm
@@ -9,7 +9,7 @@ use Email::Address qw//;
 use Email::Simple qw//;
 use POSIX qw//;
 use Date::Parse qw/str2time/;
-use PublicInbox::MID qw/mid_clean mid_compress/;
+use PublicInbox::MID qw/mid_clean/;
 use Encode qw/find_encoding/;
 my $enc_utf8 = find_encoding('UTF-8');
 our $PFX2TERM_RE = undef;
@@ -167,7 +167,7 @@ sub _extract_mid {
 	my ($self) = @_;
 
 	my $mid = $self->mime->header('Message-ID');
-	$mid ? mid_compress(mid_clean($mid)) : $mid;
+	defined $mid ? mid_clean($mid) : $mid;
 }
 
 sub mime {

t/search.t
@@ -192,7 +192,6 @@ sub filter_mids {
 	$rw_commit->();
 	$ro->reopen;
 	my $long_mid = 'last' . ('x' x 60). '@s';
-	my $long_midc = Digest::SHA::sha1_hex($long_mid);
 
 	my $long = Email::MIME->create(
 		header_str => [
@@ -232,7 +231,7 @@ sub filter_mids {
 	$ro->reopen;
 	my $t = $ro->get_thread('root@s');
 	is($t->{total}, 4, "got all 4 mesages in thread");
-	my @exp = sort($long_reply_mid, 'root@s', 'last@s', $long_midc);
+	my @exp = sort($long_reply_mid, 'root@s', 'last@s', $long_mid);
 	@res = filter_mids($t);
 	is_deeply(\@res, \@exp, "get_thread works");
 }

commit 43238d3688a51d98ce47151173aa7971e231a3bb

get rid of Message-ID compression entirely
Provide a fallback for legacy SHA-1 messages, but do not
advertise shorter URLs anymore for data portability concerns.

This fixes a regression introduced in
commit 81a9c1b476987d845b340ab9013d26cf4487cb9a
("search: disable Message-ID compression in Xapian")
which ended up breaking thread-related endpoints for
large Message-IDs, as lookups on the SHA-1 message no longer
worked.

lib/PublicInbox/ExtMsg.pm
@@ -5,7 +5,7 @@ use strict;
 use warnings;
 use URI::Escape qw(uri_escape_utf8);
 use PublicInbox::Hval;
-use PublicInbox::MID qw/mid_compress mid2path/;
+use PublicInbox::MID qw/mid2path/;
 
 # TODO: user-configurable
 our @EXT_URL = (

lib/PublicInbox/Feed.pm
@@ -9,7 +9,7 @@ use Date::Parse qw(strptime);
 use PublicInbox::Hval;
 use PublicInbox::GitCatFile;
 use PublicInbox::View;
-use PublicInbox::MID qw/mid_clean mid_compress mid2path/;
+use PublicInbox::MID qw/mid_clean mid2path/;
 use POSIX qw/strftime/;
 use constant {
 	DATEFMT => '%Y-%m-%dT%H:%M:%SZ', # atom standard
@@ -393,8 +393,7 @@ sub add_topic {
 			my $mime = do_cat_mail($git, $path) or return 0;
 			$header_obj = $mime->header_obj;
 		}
-		my $mid = $header_obj->header('Message-ID');
-		$mid = mid_compress(mid_clean($mid));
+		my $mid = mid_clean($header_obj->header('Message-ID'));
 		$u = $enc_utf8->decode($u);
 		push @$order, [ $mid, $ts, $u, $subj, $topic ];
 		return 1;

lib/PublicInbox/Hval.pm
@@ -8,7 +8,7 @@ use warnings;
 use fields qw(raw href);
 use Encode qw(find_encoding);
 use URI::Escape qw(uri_escape_utf8);
-use PublicInbox::MID qw/mid_clean mid_compress/;
+use PublicInbox::MID qw/mid_clean/;
 
 my $enc_ascii = find_encoding('us-ascii');
 
@@ -27,7 +27,7 @@ sub new {
 sub new_msgid {
 	my ($class, $msgid, $no_compress) = @_;
 	$msgid = mid_clean($msgid);
-	$class->new($msgid, $no_compress ? $msgid : mid_compress($msgid));
+	$class->new($msgid, $msgid);
 }
 
 sub new_oneline {

lib/PublicInbox/Mbox.pm
@@ -4,7 +4,7 @@
 package PublicInbox::Mbox;
 use strict;
 use warnings;
-use PublicInbox::MID qw/mid_compress mid2path/;
+use PublicInbox::MID qw/mid2path/;
 require Email::Simple;
 
 sub thread_mbox {
@@ -69,7 +69,7 @@ sub emit_mbox {
 	$fh = PublicInbox::MboxGz->new($fh) if $sfx;
 
 	require PublicInbox::GitCatFile;
-	my $mid = mid_compress($ctx->{mid});
+	my $mid = $ctx->{mid};
 	my $git = PublicInbox::GitCatFile->new($ctx->{git_dir});
 	my %opts = (offset => 0);
 	my $nr;

lib/PublicInbox/View.pm
@@ -161,7 +161,7 @@ sub thread_html {
 
 sub emit_thread_html {
 	my ($cb, $ctx, $foot, $srch) = @_;
-	my $mid = mid_compress($ctx->{mid});
+	my $mid = $ctx->{mid};
 	my $res = $srch->get_thread($mid);
 	my $msgs = load_results($res);
 	my $nr = scalar @$msgs;
@@ -447,7 +447,7 @@ sub headers_to_html_header {
 sub thread_inline {
 	my ($dst, $ctx, $cur, $full_pfx) = @_;
 	my $srch = $ctx->{srch};
-	my $mid = mid_compress(mid_clean($cur->header('Message-ID')));
+	my $mid = mid_clean($cur->header('Message-ID'));
 	my $res = $srch->get_thread($mid);
 	my $nr = $res->{total};
 
@@ -465,7 +465,7 @@ sub thread_inline {
 		seen => { $subj => 1 },
 		srch => $srch,
 		cur => $mid,
-		parent_cmp => $parent ? mid_compress($parent) : '',
+		parent_cmp => defined $parent ? $parent : '',
 		parent => $parent,
 	};
 	for (thread_results(load_results($res))->rootset) {
@@ -683,16 +683,15 @@ sub _inline_header {
 	my $pfx = INDENT x $level;
 
 	my $cur = $state->{cur};
-	my $mid = $mime->header('Message-ID');
+	my $mid = mid_clean($mime->header('Message-ID'));
 	my $f = $mime->header('X-PI-From');
 	my $d = _msg_date($mime);
 	$f = PublicInbox::Hval->new($f);
 	$d = PublicInbox::Hval->new($d);
 	$f = $f->as_html;
 	$d = $d->as_html . ' UTC';
-	my $midc = mid_compress(mid_clean($mid));
 	if ($cur) {
-		if ($cur eq $midc) {
+		if ($cur eq $mid) {
 			delete $state->{cur};
 			$$dst .= "$pfx` <b><a\nid=\"r\"\nhref=\"#t\">".
 				 "[this message]</a></b> by $f @ $d\n";
@@ -700,7 +699,7 @@ sub _inline_header {
 			return;
 		}
 	} else {
-		$state->{next_msg} ||= $midc;
+		$state->{next_msg} ||= $mid;
 	}
 
 	# Subject is never undef, this mail was loaded from

lib/PublicInbox/WWW.pm
@@ -116,8 +116,18 @@ sub invalid_list {
 sub invalid_list_mid {
 	my ($ctx, $listname, $mid) = @_;
 	my $ret = invalid_list($ctx, $listname, $mid);
-	$ctx->{mid} = uri_unescape($mid) unless $ret;
-	$ret;
+	return $ret if $ret;
+
+	$ctx->{mid} = $mid = uri_unescape($mid);
+	if ($mid =~ /\A[a-f0-9]{40}\z/) {
+		if ($mid = mid2blob($ctx)) {
+			require Email::Simple;
+			use PublicInbox::MID qw/mid_clean/;
+			$mid = Email::Simple->new($mid);
+			$ctx->{mid} = mid_clean($mid->header('Message-ID'));
+		}
+	}
+	undef;
 }
 
 # /$LISTNAME/new.atom                     -> Atom feed, includes replies