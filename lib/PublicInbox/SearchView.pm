nojb/public-inbox

commit 4c2c2325d2948ec5340e2fcafbee798cf568f5fd

rename 'GitCatFile' package to 'Git'
We'll be using it for more than just cat-file.
Adding a `popen' API for internal use allows us to save a bunch
of code in other places.

lib/PublicInbox/SearchView.pm
@@ -10,6 +10,7 @@ use PublicInbox::Hval;
 use PublicInbox::View;
 use PublicInbox::MID qw(mid2path mid_clean);
 use Email::MIME;
+require PublicInbox::Git;
 our $LIM = 50;
 
 sub sres_top_html {
@@ -169,12 +170,10 @@ sub tdump {
 		$th->order(*PublicInbox::View::rsort_ts);
 	}
 
-	require PublicInbox::GitCatFile;
-	my $git = PublicInbox::GitCatFile->new($ctx->{git_dir});
+	my $git = $ctx->{git} ||= PublicInbox::Git->new($ctx->{git_dir});
 	my $state = { ctx => $ctx, anchor_idx => 0, pct => \%pct };
 	$ctx->{searchview} = 1;
 	tdump_ent($fh, $git, $state, $_, 0) for $th->rootset;
-	$git = undef;
 	Email::Address->purge_cache;
 
 	$fh->write(search_nav_bot($mset, $q). "\n\n" .
@@ -236,8 +235,7 @@ sub html_start {
 sub adump {
 	my ($cb, $mset, $q, $ctx) = @_;
 	my $fh = $cb->([ 200, ['Content-Type' => 'application/atom+xml']]);
-	require PublicInbox::GitCatFile;
-	my $git = PublicInbox::GitCatFile->new($ctx->{git_dir});
+	my $git = $ctx->{git_dir} ||= PublicInbox::Git->new($ctx->{git_dir});
 	my $feed_opts = PublicInbox::Feed::get_feedopts($ctx);
 	my $x = PublicInbox::Hval->new_oneline($q->{q})->as_html;
 	$x = qq{$x - search results};
@@ -251,7 +249,6 @@ sub adump {
 		$x = mid2path($x);
 		PublicInbox::Feed::add_to_feed($feed_opts, $fh, $x, $git);
 	}
-	$git = undef;
 	PublicInbox::Feed::end_feed($fh);
 }
 

MANIFEST
@@ -21,7 +21,7 @@ examples/public-inbox.psgi
 lib/PublicInbox/Config.pm
 lib/PublicInbox/Feed.pm
 lib/PublicInbox/Filter.pm
-lib/PublicInbox/GitCatFile.pm
+lib/PublicInbox/Git.pm
 lib/PublicInbox/Hval.pm
 lib/PublicInbox/MDA.pm
 lib/PublicInbox/View.pm

lib/PublicInbox/ExtMsg.pm
@@ -72,21 +72,10 @@ sub ext_msg {
 	my $path = "HEAD:" . mid2path($mid);
 
 	foreach my $n (@nox) {
-		my @cmd = ('git', "--git-dir=$n->{git_dir}", 'cat-file',
-			   '-t', $path);
-		my $pid = open my $fh, '-|';
-		defined $pid or die "fork failed: $!\n";
-
-		if ($pid == 0) {
-			open STDERR, '>', '/dev/null'; # ignore errors
-			exec @cmd or die "exec failed: $!\n";
-		} else {
-			my $type = eval { local $/; <$fh> };
-			close $fh;
-			if ($? == 0 && $type eq "blob\n") {
-				return r302($n->{url}, $mid);
-			}
-		}
+		# TODO: reuse existing PublicInbox::Git objects to save forks
+		my $git = PublicInbox::Git->new($n->{git_dir});
+		my (undef, $type, undef) = $git->check($path);
+		return r302($n->{url}, $mid) if ($type eq 'blob');
 	}
 
 	# fall back to partial MID matching

lib/PublicInbox/Feed.pm
@@ -9,7 +9,7 @@ use Email::Address;
 use Email::MIME;
 use Date::Parse qw(strptime);
 use PublicInbox::Hval;
-use PublicInbox::GitCatFile;
+use PublicInbox::Git;
 use PublicInbox::View;
 use PublicInbox::MID qw/mid_clean mid2path/;
 use POSIX qw/strftime/;
@@ -66,7 +66,7 @@ sub emit_atom {
 	my $max = $ctx->{max} || MAX_PER_PAGE;
 	my $feed_opts = get_feedopts($ctx);
 	my $x = atom_header($feed_opts);
-	my $git = PublicInbox::GitCatFile->new($ctx->{git_dir});
+	my $git = $ctx->{git} ||= PublicInbox::Git->new($ctx->{git_dir});
 	each_recent_blob($ctx, sub {
 		my ($path, undef, $ts) = @_;
 		if (defined $x) {
@@ -75,7 +75,6 @@ sub emit_atom {
 		}
 		add_to_feed($feed_opts, $fh, $path, $git);
 	});
-	$git = undef; # destroy pipes
 	end_feed($fh);
 }
 
@@ -105,11 +104,10 @@ sub emit_atom_thread {
 	$feed_opts->{url} = $html_url;
 	$feed_opts->{emit_header} = 1;
 
-	my $git = PublicInbox::GitCatFile->new($ctx->{git_dir});
+	my $git = $ctx->{git} ||= PublicInbox::Git->new($ctx->{git_dir});
 	foreach my $msg (@{$res->{msgs}}) {
 		add_to_feed($feed_opts, $fh, mid2path($msg->mid), $git);
 	}
-	$git = undef; # destroy pipes
 	end_feed($fh);
 }
 
@@ -167,7 +165,7 @@ sub emit_html_index {
 
 sub emit_index_nosrch {
 	my ($ctx, $state, $fh) = @_;
-	my $git = PublicInbox::GitCatFile->new($ctx->{git_dir});
+	my $git = $ctx->{git} ||= PublicInbox::Git->new($ctx->{git_dir});
 	my (undef, $last) = each_recent_blob($ctx, sub {
 		my ($path, $commit, $ts, $u, $subj) = @_;
 		$state->{first} ||= $commit;
@@ -219,14 +217,11 @@ sub each_recent_blob {
 	# get recent messages
 	# we could use git log -z, but, we already know ssoma will not
 	# leave us with filenames with spaces in them..
-	my @cmd = ('git', "--git-dir=$ctx->{git_dir}",
-			qw/log --no-notes --no-color --raw -r
-			   --abbrev=16 --abbrev-commit/,
-			"--format=%h%x00%ct%x00%an%x00%s%x00");
-	push @cmd, $range;
-
-	my $pid = open(my $log, '-|', @cmd) or
-		die('open `'.join(' ', @cmd) . " pipe failed: $!\n");
+	my $git = $ctx->{git} ||= PublicInbox::Git->new($ctx->{git_dir});
+	my $log = $git->popen(qw/log --no-notes --no-color --raw -r
+				--abbrev=16 --abbrev-commit/,
+				"--format=%h%x00%ct%x00%an%x00%s%x00",
+				$range);
 	my %deleted; # only an optimization at this point
 	my $last;
 	my $nr = 0;

lib/PublicInbox/Git.pm
@@ -6,7 +6,7 @@
 # This is based on code in Git.pm which is GPLv2, but modified to avoid
 # dependence on environment variables for compatibility with mod_perl.
 # There are also API changes to simplify our usage and data set.
-package PublicInbox::GitCatFile;
+package PublicInbox::Git;
 use strict;
 use warnings;
 use POSIX qw(dup2);
@@ -119,6 +119,16 @@ sub fail {
 	my ($self, $msg) = @_;
 	cleanup($self);
 	die $msg;
+}
+
+sub popen {
+	my ($self, @cmd) = @_;
+	my $mode = '-|';
+	$mode = shift @cmd if ($cmd[0] eq '|-');
+	@cmd = ('git', "--git-dir=$self->{git_dir}", @cmd);
+	my $pid = open my $fh, $mode, @cmd or
+		die('open `'.join(' ', @cmd) . " pipe failed: $!\n");
+	$fh;
 }
 
 sub cleanup {

lib/PublicInbox/Mbox.pm
@@ -86,9 +86,9 @@ sub emit_mbox {
 	my $fh = $response->([200, ['Content-Type' => "application/$type"]]);
 	$fh = PublicInbox::MboxGz->new($fh) if $sfx;
 
-	require PublicInbox::GitCatFile;
+	require PublicInbox::Git;
 	my $mid = $ctx->{mid};
-	my $git = PublicInbox::GitCatFile->new($ctx->{git_dir});
+	my $git = $ctx->{git} ||= PublicInbox::Git->new($ctx->{git_dir});
 	my %opts = (offset => 0);
 	my $nr;
 	do {

lib/PublicInbox/NNTP.pm
@@ -9,7 +9,7 @@ use base qw(Danga::Socket);
 use fields qw(nntpd article rbuf ng long_res);
 use PublicInbox::Search;
 use PublicInbox::Msgmap;
-use PublicInbox::GitCatFile;
+use PublicInbox::Git;
 use PublicInbox::MID qw(mid2path);
 use Email::MIME;
 use Data::Dumper qw(Dumper);

lib/PublicInbox/NewsGroup.pm
@@ -10,7 +10,7 @@ use Scalar::Util qw(weaken);
 require Danga::Socket;
 require PublicInbox::Msgmap;
 require PublicInbox::Search;
-require PublicInbox::GitCatFile;
+require PublicInbox::Git;
 
 sub new {
 	my ($class, $name, $git_dir, $address) = @_;
@@ -32,7 +32,7 @@ sub defer_weaken {
 sub gcf {
 	my ($self) = @_;
 	$self->{gcf} ||= eval {
-		my $gcf = PublicInbox::GitCatFile->new($self->{git_dir});
+		my $gcf = PublicInbox::Git->new($self->{git_dir});
 
 		# git repos may be repacked and old packs unlinked
 		defer_weaken($self, 'gcf');

commit 40ded418d2786892d3e925df10b1e4307bcec018

searchview: fix up Atom feed in search results
Oops :x  We need better testing...
Fixes: commit 4c2c2325d2948ec5340e2fcafbee798cf568f5fd
("rename 'GitCatFile' package to 'Git'")

lib/PublicInbox/SearchView.pm
@@ -235,7 +235,7 @@ sub html_start {
 sub adump {
 	my ($cb, $mset, $q, $ctx) = @_;
 	my $fh = $cb->([ 200, ['Content-Type' => 'application/atom+xml']]);
-	my $git = $ctx->{git_dir} ||= PublicInbox::Git->new($ctx->{git_dir});
+	my $git = $ctx->{git} ||= PublicInbox::Git->new($ctx->{git_dir});
 	my $feed_opts = PublicInbox::Feed::get_feedopts($ctx);
 	my $x = PublicInbox::Hval->new_oneline($q->{q})->as_html;
 	$x = qq{$x - search results};