17e9e2b0df6d445883b2822c49c971fb9abe3c0e
Eric Wong <e@80x24.org>
2016-02-28 22:40:22 +0000

    examples/cgi-webrick.rb: set CGIPathEnv, update comments
    webrick clears PATH otherwise, and we rely on git commands.

--- examples/cgi-webrick.rb
+++ examples/cgi-webrick.rb
@@ -1,13 +1,16 @@
 #!/usr/bin/env ruby
 # Sample configuration using WEBrick, mainly intended dev/testing
 # for folks familiar with Ruby and not various Perl webserver
-# deployment options.
+# deployment options.  For those familiar with Perl web servers,
+# plackup(1) is recommended for development and public-inbox-httpd(1)
+# is our production deployment server.
 require 'webrick'
 require 'logger'
 options = {
   :BindAddress => '127.0.0.1',
   :Port => 8080,
   :Logger => Logger.new($stderr),
+  :CGIPathEnv => ENV['PATH'], # need to run 'git' commands
   :AccessLog => [
     [ Logger.new($stdout), WEBrick::AccessLog::COMBINED_LOG_FORMAT ]
   ],
