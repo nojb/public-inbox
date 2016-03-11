nojb/public-inbox

commit ea1b6cbd422b7e392cdb6979837a42679b0f180f

daemon: allow using IO::Socket::IP over INET6
IO::Socket::IP is bundled with newer versions of Perl,
so it is more likely to be available.  There should
be no differences between these with our use cases.

lib/PublicInbox/Daemon.pm
@@ -68,8 +68,11 @@ sub daemon_prepare ($) {
 			}
 			$o{Local} = delete $o{Peer};
 		} else {
-			$sock_pkg = 'IO::Socket::INET6'; # works for IPv4, too
-			eval "use $sock_pkg";
+			# both work for IPv4, too
+			for $sock_pkg (qw(IO::Socket::IP IO::Socket::INET6)) {
+				eval "use $sock_pkg";
+				$@ or last;
+			}
 			die $@ if $@;
 			%o = (LocalAddr => $l, ReuseAddr => 1, Proto => 'tcp');
 		}

commit d5fce291ff67459ccf2939afea0ea6a1157869d8

daemon: fixup usage of the '-l' switch with IP/INET6 sockets
We need to ensure $sock_pkg is preserved outside of the loop.
The variable passed to "for" or "foreach" is implicitly local
and restores the previous value when the loop exits.  This is
documented in the perlsyn manpage in the "Foreach Loops"
section.

Fixes: ea1b6cbd422b ("daemon: allow using IO::Socket::IP over INET6")

lib/PublicInbox/Daemon.pm
@@ -69,7 +69,8 @@ sub daemon_prepare ($) {
 			$o{Local} = delete $o{Peer};
 		} else {
 			# both work for IPv4, too
-			for $sock_pkg (qw(IO::Socket::IP IO::Socket::INET6)) {
+			for (qw(IO::Socket::IP IO::Socket::INET6)) {
+				$sock_pkg = $_;
 				eval "use $sock_pkg";
 				$@ or last;
 			}
@@ -79,7 +80,7 @@ sub daemon_prepare ($) {
 		$o{Listen} = 1024;
 		my $prev = umask 0000;
 		my $s = eval { $sock_pkg->new(%o) };
-		warn "error binding $l: $!\n" unless $s;
+		warn "error binding $l: $! ($@)\n" unless $s;
 		umask $prev;
 
 		if ($s) {