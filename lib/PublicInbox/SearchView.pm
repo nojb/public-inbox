nojb/public-inbox

commit 56164afc203489920ca9ef4b7407b39625acfa9f

view: allow topics to be "bumped" by new replies
This ought to prevent new replies from getting lost for readers
relying on the WWW index interface.

lib/PublicInbox/View.pm
@@ -843,13 +843,6 @@ sub sort_ts {
 	} @_;
 }
 
-sub rsort_ts {
-	sort {
-		(eval { $b->topmost->message->header('X-PI-TS') } || 0) <=>
-		(eval { $a->topmost->message->header('X-PI-TS') } || 0)
-	} @_;
-}
-
 # accumulate recent topics if search is supported
 # returns 1 if done, undef if not
 sub add_topic {
@@ -884,16 +877,18 @@ sub add_topic {
 	add_topic($state, $node->next, $level);
 }
 
-sub dump_topics {
+sub emit_topics {
 	my ($state) = @_;
 	my $order = $state->{order};
 	my $subjs = $state->{subjs};
 	my $latest = $state->{latest};
-	return "\n[No topics in range]</pre>" unless (scalar @$order);
-	my $dst = '';
+	my $fh = $state->{fh};
+	return $fh->write("\n[No topics in range]</pre>") unless scalar @$order;
 	my $pfx;
 	my $prev = 0;
 	my $prev_attr = '';
+	my $cur;
+	my @recent;
 	while (defined(my $info = shift @$order)) {
 		my ($level, $subj) = @$info;
 		my $n = delete $subjs->{$subj};
@@ -902,8 +897,16 @@ sub dump_topics {
 		$subj = PublicInbox::Hval->new($subj)->as_html;
 		$pfx = indent_for($level);
 		my $nl = $level == $prev ? "\n" : '';
+		if ($nl && $cur) {
+			push @recent, $cur;
+			$cur = undef;
+		}
+		$cur ||= [ $ts, '' ];
 		my $dot = $level == 0 ? '' : '` ';
-		$dst .= "$nl$pfx$dot<a\nhref=\"$mid/t/#u\"><b>$subj</b></a>\n";
+		$cur->[0] = $ts if $ts > $cur->[0];
+		$cur->[1] .= "$nl$pfx$dot<a\nhref=\"$mid/t/#u\"><b>";
+		$cur->[1] .= $subj;
+		$cur->[1] .= "</b></a>\n";
 
 		$ts = fmt_ts($ts);
 		my $attr = " $ts UTC";
@@ -916,11 +919,13 @@ sub dump_topics {
 			my $mbox = qq(<a\nhref="$mid/t.mbox.gz">mbox.gz</a>);
 			my $atom = qq(<a\nhref="$mid/t.atom">Atom</a>);
 			$pfx .= INDENT if $level > 0;
-			$dst .= $pfx . $attr . $n . " - $mbox / $atom\n";
+			$cur->[1] .= $pfx . $attr . $n . " - $mbox / $atom\n";
 			$prev_attr = $attr;
 		}
 	}
-	$dst .= '</pre>';
+	push @recent, $cur if $cur;
+	@recent = map { $_->[1] } sort { $b->[0] <=> $a->[0] } @recent;
+	$fh->write(join('', @recent) . '</pre>');
 }
 
 sub emit_index_topics {
@@ -936,13 +941,13 @@ sub emit_index_topics {
 		my $sres = $state->{srch}->query('', \%opts);
 		my $nr = scalar @{$sres->{msgs}} or last;
 
-		for (rsort_ts(thread_results(load_results($sres), 1)->rootset)){
+		for (thread_results(load_results($sres), 1)->rootset) {
 			add_topic($state, $_, 0);
 		}
 		$opts{offset} += $nr;
 	}
 
-	$state->{fh}->write(dump_topics($state));
+	emit_topics($state);
 	$opts{offset};
 }
 

commit 18a511c17dd9c4b5fad3a1e837bb8d23216bf758

searchview: deal with the removal of rsort
Oops.  While we're at it, simplify the calls to do threading
slightly by reducing the places where we touch Mail::Thread
globals.

Fixes: 56164afc2034 (view: allow topics to be "bumped" by new replies)

lib/PublicInbox/SearchView.pm
@@ -150,13 +150,8 @@ sub tdump {
 		$m;
 	} ($mset->items);
 
-	require PublicInbox::Thread;
-	my $th = PublicInbox::Thread->new(@m);
-	{
-		no warnings 'once';
-		$Mail::Thread::nosubject = 0;
-	}
-	$th->thread;
+	my @rootset;
+	my $th = PublicInbox::View::thread_results(\@m, 0, $q->{r});
 	if ($q->{r}) {
 		$th->order(sub {
 			sort { (eval { $pct{$b->topmost->messageid} } || 0)
@@ -164,9 +159,13 @@ sub tdump {
 				(eval { $pct{$a->topmost->messageid} } || 0)
 			} @_;
 		});
+		@rootset = $th->rootset;
 	} else {
-		no warnings 'once';
-		$th->order(*PublicInbox::View::rsort_ts);
+		@rootset = sort {
+			(eval { $b->topmost->message->header('X-PI-TS') } || 0)
+				<=>
+			(eval { $a->topmost->message->header('X-PI-TS') } || 0)
+		} $th->rootset;
 	}
 
 	my $git = $ctx->{git} ||= PublicInbox::Git->new($ctx->{git_dir});
@@ -178,7 +177,7 @@ sub tdump {
 		fh => $fh,
 	};
 	$ctx->{searchview} = 1;
-	tdump_ent($git, $state, $_, 0) for $th->rootset;
+	tdump_ent($git, $state, $_, 0) for @rootset;
 	PublicInbox::View::thread_adj_level($state, 0);
 	Email::Address->purge_cache;
 

lib/PublicInbox/View.pm
@@ -726,7 +726,7 @@ sub msg_timestamp {
 }
 
 sub thread_results {
-	my ($msgs, $nosubject) = @_;
+	my ($msgs, $nosubject, $nosort) = @_;
 	require PublicInbox::Thread;
 	my $th = PublicInbox::Thread->new(@$msgs);
 
@@ -740,7 +740,7 @@ sub thread_results {
 	# Keep ghosts with only a single direct child:
 	$Mail::Thread::noprune = 1;
 	$th->thread;
-	$th->order(*sort_ts);
+	$th->order(*sort_ts) unless $nosort;
 	$th
 }
 