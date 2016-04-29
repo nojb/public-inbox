nojb/public-inbox

commit c3eeaf664cf0458b075c2f23b18abeb5d720058e

http: clarify intent for persistence
We don't actually need to know if a response is chunked or
what the actual Content-Length is; we just need to know if
the PSGI app properly terminated the response so we can
handle persistent connections.

lib/PublicInbox/HTTP.pm
@@ -151,7 +151,7 @@ sub response_header_write {
 	my $proto = $env->{SERVER_PROTOCOL} or return; # HTTP/0.9 :P
 	my $status = $res->[0];
 	my $h = "$proto $status " . status_message($status) . "\r\n";
-	my ($len, $chunked);
+	my $term;
 	my $headers = $res->[1];
 
 	for (my $i = 0; $i < @$headers; $i += 2) {
@@ -159,33 +159,32 @@ sub response_header_write {
 		my $v = $headers->[$i + 1];
 		next if $k =~ /\A(?:Connection|Date)\z/i;
 
-		$len = $v if $k =~ /\AContent-Length\z/i;
-		if ($k =~ /\ATransfer-Encoding\z/i && $v =~ /\bchunked\b/i) {
-			$chunked = 1;
+		if ($k =~ /\AContent-Length\z/ ||
+		    ($k =~ /\ATransfer-Encoding\z/i && $v =~ /\bchunked\b/i)) {
+			$term = 1;
 		}
-
 		$h .= "$k: $v\r\n";
 	}
 
 	my $conn = $env->{HTTP_CONNECTION} || '';
-	my $alive = (defined($len) || $chunked) &&
+	my $alive = $term &&
 			(($proto eq 'HTTP/1.1' && $conn !~ /\bclose\b/i) ||
 			 ($conn =~ /\bkeep-alive\b/i));
 
 	$h .= 'Connection: ' . ($alive ? 'keep-alive' : 'close');
 	$h .= "\r\nDate: " . http_date() . "\r\n\r\n";
 
-	if (($len || $chunked) && $env->{REQUEST_METHOD} ne 'HEAD') {
+	if ($term && $env->{REQUEST_METHOD} ne 'HEAD') {
 		more($self, $h);
 	} else {
 		$self->write($h);
 	}
-	($alive, $chunked);
+	$alive;
 }
 
 sub response_write {
 	my ($self, $env, $res) = @_;
-	my ($alive, $chunked) = response_header_write($self, $env, $res);
+	my $alive = response_header_write($self, $env, $res);
 	my $write = sub { $self->write($_[0]) };
 	my $close = sub {
 		if ($alive) {

commit e901b85c67912a3fd8d17ac0460a57029439f71d

http: avoid corking on "Content-Length: 0" response
We must use a normal write instead of send(.., MSG_MORE)
when writing responses of "Content-Length: 0" to avoid
the corking effect MSG_MORE provides.  We only want to
cork headers if we will send a non-empty body.

Fixes: c3eeaf664cf0 ("http: clarify intent for persistence")
This needs a proper test.

lib/PublicInbox/HTTP.pm
@@ -151,7 +151,7 @@ sub response_header_write {
 	my $proto = $env->{SERVER_PROTOCOL} or return; # HTTP/0.9 :P
 	my $status = $res->[0];
 	my $h = "$proto $status " . status_message($status) . "\r\n";
-	my $term;
+	my ($len, $chunked);
 	my $headers = $res->[1];
 
 	for (my $i = 0; $i < @$headers; $i += 2) {
@@ -159,14 +159,15 @@ sub response_header_write {
 		my $v = $headers->[$i + 1];
 		next if $k =~ /\A(?:Connection|Date)\z/i;
 
-		if ($k =~ /\AContent-Length\z/ ||
-		    ($k =~ /\ATransfer-Encoding\z/i && $v =~ /\bchunked\b/i)) {
-			$term = 1;
+		$len = $v if $k =~ /\AContent-Length\z/i;
+		if ($k =~ /\ATransfer-Encoding\z/i && $v =~ /\bchunked\b/i) {
+			$chunked = 1;
 		}
 		$h .= "$k: $v\r\n";
 	}
 
 	my $conn = $env->{HTTP_CONNECTION} || '';
+	my $term = defined($len) || $chunked;
 	my $alive = $term &&
 			(($proto eq 'HTTP/1.1' && $conn !~ /\bclose\b/i) ||
 			 ($conn =~ /\bkeep-alive\b/i));
@@ -174,7 +175,7 @@ sub response_header_write {
 	$h .= 'Connection: ' . ($alive ? 'keep-alive' : 'close');
 	$h .= "\r\nDate: " . http_date() . "\r\n\r\n";
 
-	if ($term && $env->{REQUEST_METHOD} ne 'HEAD') {
+	if (($len || $chunked) && $env->{REQUEST_METHOD} ne 'HEAD') {
 		more($self, $h);
 	} else {
 		$self->write($h);

t/httpd-corner.psgi
@@ -56,6 +56,8 @@ my $app = sub {
 			$fh->write($buf);
 			$fh->close;
 		}
+	} elsif ($path eq '/empty') {
+		$code = 200;
 	}
 
 	[ $code, $h, $body ]

t/httpd-corner.t
@@ -5,6 +5,7 @@
 use strict;
 use warnings;
 use Test::More;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 foreach my $mod (qw(Plack::Util Plack::Request Plack::Builder Danga::Socket
 			HTTP::Date HTTP::Status)) {
@@ -272,6 +273,18 @@ SKIP: {
 		is($hex, sha1_hex(''), "read expected body $i");
 		$i++;
 	}
+}
+
+{
+	my $conn = conn_for($sock, 'no TCP_CORK on empty body');
+	$conn->write("GET /empty HTTP/1.1\r\nHost:example.com\r\n\r\n");
+	my $buf = '';
+	my $t0 = [ gettimeofday ];
+	until ($buf =~ /\r\n\r\n/s) {
+		$conn->sysread($buf, 4096, length($buf));
+	}
+	my $elapsed = tv_interval($t0, [ gettimeofday ]);
+	ok($elapsed < 0.190, 'no 200ms TCP cork delay on empty body');
 }
 
 {