nojb/public-inbox

commit 310819ea86ac7f85d39a2a8720c4d179c8c94277

git-http-backend: favor sysread for regular files
We do not need line buffering, here; so favor sysread to
bypass extra copies which may be done by normal read.

lib/PublicInbox/GitHTTPBackend.pm
@@ -73,7 +73,7 @@ sub serve {
 		my $n = 8192;
 		while ($len > 0) {
 			$n = $len if $len < $n;
-			my $r = read($in, $buf, $n);
+			my $r = sysread($in, $buf, $n);
 			last if (!defined($r) || $r <= 0);
 			$len -= $r;
 			$fh->write($buf);

commit 526a1e027cacf0c49092ae90b9b5d02c9d381705

git-http-backend: use real lseek for Content-Range
Since we use sysread, we must use sysseek for symmetry although
PerlIO may be doing a real lseek with "seek", anyways.

Fixes: 310819ea86ac ("git-http-backend: favor sysread for regular files")

lib/PublicInbox/GitHTTPBackend.pm
@@ -130,7 +130,7 @@ sub prepare_range {
 		if ($len <= 0) {
 			$code = 416;
 		} else {
-			seek($in, $beg, SEEK_SET) or return [ 500, [], [] ];
+			sysseek($in, $beg, SEEK_SET) or return [ 500, [], [] ];
 			push @$h, qw(Accept-Ranges bytes Content-Range);
 			push @$h, "bytes $beg-$end/$size";
 