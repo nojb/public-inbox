nojb/public-inbox

commit 395406118cb22b4cfa3a83c2b3a3a9aa2627b708

httpd/async: prevent circular reference
We must avoid circular references which can cause leaks in
long-running processes.  This callback is dangerous since
it may never be called to properly terminate everything.

lib/PublicInbox/HTTPD/Async.pm
@@ -10,6 +10,7 @@ use strict;
 use warnings;
 use base qw(Danga::Socket);
 use fields qw(cb cleanup);
+use Scalar::Util qw(weaken);
 require PublicInbox::EvCleanup;
 
 sub new {
@@ -25,11 +26,12 @@ sub new {
 
 sub async_pass {
 	my ($self, $io, $fh, $bref) = @_;
-	my $restart_read = sub { $self->watch_read(1) };
 	# In case the client HTTP connection ($io) dies, it
 	# will automatically close this ($self) object.
 	$io->{forward} = $self;
 	$fh->write($$bref);
+	weaken($self);
+	my $restart_read = sub { $self->watch_read(1) };
 	$self->{cb} = sub {
 		my $r = sysread($self->{sock}, $$bref, 8192);
 		if ($r) {

commit aa4e04c633ee857cb372d7e55c545d7a7b29d2eb

httpd/async: do not needlessly weaken
The restart_read callback has no chance of circular reference,
and weakening $self before we create it can cause $self to
be undefined inside the callback (seen during stress testing).

Fixes: 395406118cb2 ("httpd/async: prevent circular reference")

lib/PublicInbox/HTTPD/Async.pm
@@ -24,14 +24,19 @@ sub new {
 	$self;
 }
 
+sub restart_read_cb ($) {
+	my ($self) = @_;
+	sub { $self->watch_read(1) }
+}
+
 sub async_pass {
 	my ($self, $io, $fh, $bref) = @_;
 	# In case the client HTTP connection ($io) dies, it
 	# will automatically close this ($self) object.
 	$io->{forward} = $self;
 	$fh->write($$bref);
+	my $restart_read = restart_read_cb($self);
 	weaken($self);
-	my $restart_read = sub { $self->watch_read(1) };
 	$self->{cb} = sub {
 		my $r = sysread($self->{sock}, $$bref, 8192);
 		if ($r) {