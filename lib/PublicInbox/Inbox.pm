nojb/public-inbox

commit 31a6ff1221fe70f309ac2476024ebb7953a4e477

inbox: ensure we do not show leading "From " lines
Some messages will be misimported due to an old bug,
clean them up and ensure we do not propagate the mistake.

Followup-to: a0c07cba0e5d ("mda: drop leading "From " lines again")

lib/PublicInbox/Inbox.pm
@@ -99,7 +99,9 @@ sub nntp_usable {
 sub msg_by_path ($$;$) {
 	my ($self, $path, $ref) = @_;
 	# TODO: allow other refs:
-	git($self)->cat_file('HEAD:'.$path, $ref);
+	my $str = git($self)->cat_file('HEAD:'.$path, $ref);
+	$$str =~ s/\A[\r\n]*From [^\r\n]*\r?\n//s;
+	$str;
 }
 
 sub msg_by_mid ($$;$) {

lib/PublicInbox/SearchIdx.pm
@@ -311,6 +311,8 @@ sub do_cat_mail {
 	my ($git, $blob, $sizeref) = @_;
 	my $mime = eval {
 		my $str = $git->cat_file($blob, $sizeref);
+		# fixup bugs from import:
+		$$str =~ s/\A[\r\n]*From [^\r\n]*\r?\n//s;
 		Email::MIME->new($str);
 	};
 	$@ ? undef : $mime;

commit 62e690b87d03ab1fa1829f99e37c63707c64a5c4

inbox: avoid trying s// on undef
Oops, I guess I'm trigger-happy today.

Fixes: 31a6ff1221fe ("inbox: ensure we do not show leading "From " lines")

lib/PublicInbox/Inbox.pm
@@ -100,7 +100,7 @@ sub msg_by_path ($$;$) {
 	my ($self, $path, $ref) = @_;
 	# TODO: allow other refs:
 	my $str = git($self)->cat_file('HEAD:'.$path, $ref);
-	$$str =~ s/\A[\r\n]*From [^\r\n]*\r?\n//s;
+	$$str =~ s/\A[\r\n]*From [^\r\n]*\r?\n//s if $str;
 	$str;
 }
 