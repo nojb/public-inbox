2fb6b0150b8a8fc49385d21d7fd1f8a1deff5ebf
Eric Wong <e@80x24.org>
2016-07-05 13:05:41 +0000

    daemon: disable USR2/TTIN/TTOU/WINCH in workers
    If using a master/worker setup, a careless user could be trying
    to signal all processes using "killall".  This may trigger bad
    side-effects; but try to limit the side-effects as much as
    possible.

--- lib/PublicInbox/Daemon.pm
+++ lib/PublicInbox/Daemon.pm
@@ -449,7 +449,8 @@ sub daemon_loop ($$) {
 	$SIG{QUIT} = $SIG{INT} = $SIG{TERM} = *worker_quit;
 	$SIG{USR1} = *reopen_logs;
 	$SIG{HUP} = $refresh;
-	$SIG{$_} = 'DEFAULT' for qw(CHLD USR2 TTIN TTOU WINCH);
+	$SIG{CHLD} = 'DEFAULT';
+	$SIG{$_} = 'IGNORE' for qw(USR2 TTIN TTOU WINCH);
 	# this calls epoll_create:
 	@listeners = map {
 		PublicInbox::Listener->new($_, $post_accept)
