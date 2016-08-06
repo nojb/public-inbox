nojb/public-inbox

commit 6ef9b216156c4bea4bbdd3e0bed3caf85e59a520

view: use <hr> to delineate in /$MID/T/ view
The sacrifice in vertical space might be worth it to improve
ease-of-reading, as it's unreasonable to expect an entire
message thread to be able to fit into a single window.

https://public-inbox.org/git/20160805093544.scvl4yshkfg2l26p@sigill.intra.peff.net/

Cc: Jeff King <peff@peff.net>

lib/PublicInbox/View.pm
@@ -180,7 +180,7 @@ sub index_entry {
 		$rv .= " <a\nhref=#r$id>$ctx->{s_nr}</a>";
 	}
 
-	$rv .= $more ? "\n\n" : "\n";
+	$rv .= $more ? '</pre><hr><pre>' : '</pre>';
 }
 
 sub pad_link ($$;$) {

commit b478f5be70f5e210347c7692333bb65ad3316f72

view: do not introduce excessive </pre> in $MID/t/ view
When using <ul><li>..., we already setup <pre> tags
in thread_index_entry, so having an extra </pre> tag
causes validation errors.

Fixes: 6ef9b216156c ("view: use <hr> to delineate in /$MID/T/ view")

lib/PublicInbox/View.pm
@@ -164,13 +164,17 @@ sub index_entry {
 		"<a\nhref=\"$mhref\">permalink</a>" .
 		" <a\nhref=\"${mhref}raw\">raw</a>" .
 		" <a\nhref=\"${mhref}#R\">reply</a>";
+
+	my $hr;
 	if (my $pct = $ctx->{pct}) { # used by SearchView.pm
 		$rv .= "\t[relevance $pct->{$mid_raw}%]";
+		$hr = 1;
 	} elsif ($mapping) {
 		my $threaded = 'threaded';
 		my $flat = 'flat';
 		my $end = '';
 		if ($ctx->{flat}) {
+			$hr = 1;
 			$flat = "<b>$flat</b>";
 		} else {
 			$threaded = "<b>$threaded</b>";
@@ -180,7 +184,8 @@ sub index_entry {
 		$rv .= " <a\nhref=#r$id>$ctx->{s_nr}</a>";
 	}
 
-	$rv .= $more ? '</pre><hr><pre>' : '</pre>';
+	$rv .= $more ? '</pre><hr><pre>' : '</pre>' if $hr;
+	$rv;
 }
 
 sub pad_link ($$;$) {