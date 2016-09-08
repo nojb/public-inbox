f9663d67d1b228b6ceaeaab8329968b0d7e445cb
Eric Wong <e@80x24.org>
2016-09-08 22:42:42 +0000

    import: run "git gc --auto" when done
    We need to prevent excessive repository growth for
    public-inbox-watch and public-inbox-mda users.

--- lib/PublicInbox/Import.pm
+++ lib/PublicInbox/Import.pm
@@ -240,6 +240,7 @@ sub done {
 			my $s = PublicInbox::SearchIdx->new($inbox);
 			$s->index_sync({ ref => $self->{ref} });
 		};
+		run_die([@cmd, qw(gc --auto)], undef);
 	}
 
 	my $lockfh = delete $self->{lockfh} or die "BUG: not locked: $!";
