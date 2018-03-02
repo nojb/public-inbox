nojb/public-inbox

commit 427245acacaf04a882d5524e662075909b96905b

evcleanup: ensure deferred close from timers are handled ASAP
Danga::Socket defers close() syscalls until the end of the event
loop to avoid FD recycling.  Unfortunately, this is dependent on
IO events firing and waking the process up from
poll/kevent/epoll_wait.

Without any I/O activity, a socket could remain in the
@Danga::Socket::ToClose array indefinitely.  Thus, we will
trigger a fake IO event after running all timers to trigger
the deferred close in Danga::Socket::PostEventLoop.

lib/PublicInbox/EvCleanup.pm
@@ -30,9 +30,19 @@ sub _run_all ($) {
 	$_->() foreach @$run;
 }
 
+# ensure Danga::Socket::ToClose fires after timers fire
+sub _asap_close () { $asapq->[1] ||= _asap_timer() }
+
 sub _run_asap () { _run_all($asapq) }
-sub _run_next () { _run_all($nextq) }
-sub _run_later () { _run_all($laterq) }
+sub _run_next () {
+	_run_all($nextq);
+	_asap_close();
+}
+
+sub _run_later () {
+	_run_all($laterq);
+	_asap_close();
+}
 
 # Called by Danga::Socket
 sub event_write {

commit 76ecc4b349fd0c41ac9fdabaf501d33cd2049da1

evcleanup: do not create event loop if nothing was registered
This was creating an unnecessary epoll descriptor via
Danga::Socket when using V2Writable to import a mbox.  That
said, there should probably be better way of detecting whether
or not we're inside a Danga::Socket event loop.

Fixes: 427245acacaf04a8
       ("evcleanup: ensure deferred close from timers are handled ASAP")

lib/PublicInbox/EvCleanup.pm
@@ -79,8 +79,8 @@ sub later ($) {
 
 END {
 	_run_asap();
-	_run_next();
-	_run_later();
+	_run_all($nextq);
+	_run_all($laterq);
 }
 
 1;