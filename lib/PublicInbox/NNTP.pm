nojb/public-inbox

commit 3fc411c772a21d8faeec3fde32b101bc86190ddb

search: drop pointless range processors for Unix timestamp
The Unix timestamp isn't meaningful for users searching,
we will start indexing the YYYYMMDD date stamp which may
use StringValueRangeProcessor, instead.

lib/PublicInbox/Search.pm
@@ -179,8 +179,6 @@ sub qp {
 	$qp->set_database($self->{xdb});
 	$qp->set_stemmer($self->stemmer);
 	$qp->set_stemming_strategy(STEM_SOME);
-	$qp->add_valuerangeprocessor($self->ts_range_processor);
-	$qp->add_valuerangeprocessor($self->date_range_processor);
 
 	while (my ($name, $prefix) = each %bool_pfx_external) {
 		$qp->add_boolean_prefix($name, $prefix);
@@ -203,14 +201,6 @@ sub qp {
 	}
 
 	$self->{query_parser} = $qp;
-}
-
-sub ts_range_processor {
-	$_[0]->{tsrp} ||= Search::Xapian::NumberValueRangeProcessor->new(TS);
-}
-
-sub date_range_processor {
-	$_[0]->{drp} ||= Search::Xapian::DateValueRangeProcessor->new(TS);
 }
 
 sub num_range_processor {

commit b212aee7e13c460b73a3632458ae96c39d9eac97

nntp: fix NEWNEWS command
I guess nobody uses this command (slrnpull does not), and
the breakage was not noticed until I started writing new
tests for multi-MID handling.

Fixes: 3fc411c772a21d8f ("search: drop pointless range processors for Unix timestamp")

lib/PublicInbox/NNTP.pm
@@ -336,7 +336,7 @@ sub cmd_newnews ($$$$;$$) {
 	long_response($self, 0, long_response_limit, sub {
 		my ($i) = @_;
 		my $srch = $srch[0];
-		my $res = $srch->query($ts, $opts);
+		my $res = $srch->query_ts($ts, $opts);
 		my $msgs = $res->{msgs};
 		if (my $nr = scalar @$msgs) {
 			more($self, '<' .

lib/PublicInbox/Search.pm
@@ -330,6 +330,20 @@ sub query_xover {
 	_do_enquire($self, $query, $opts);
 }
 
+sub query_ts {
+	my ($self, $ts, $opts) = @_;
+	my $qp = $self->{qp_ts} ||= eval {
+		my $q = Search::Xapian::QueryParser->new;
+		$q->set_database($self->{skel} || $self->{xdb});
+		$q->add_valuerangeprocessor(
+			Search::Xapian::NumberValueRangeProcessor->new(TS));
+		$q
+	};
+	my $query = $qp->parse_query($ts, QP_FLAGS);
+	$opts->{enquire} = enquire_skel($self);
+	_do_enquire($self, $query, $opts);
+}
+
 sub lookup_skeleton {
 	my ($self, $mid) = @_;
 	my $skel = $self->{skel} or return lookup_message($self, $mid);

t/nntpd.t
@@ -228,6 +228,8 @@ EOF
 	is_deeply($n->xhdr(qw(list-id 1-)), {},
 		 'XHDR on invalid header returns empty');
 
+	my $mids = $n->newnews(0, '*');
+	is_deeply($mids, ['<nntp@example.com>'], 'NEWNEWS works');
 	{
 		my $t0 = time;
 		my $date = $n->date;