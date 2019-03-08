089cca37fa03641138d229dc5dbe76b382fc200f
Eric Wong <e@80x24.org>
2019-03-08 22:11:02 +0000

    config: ignore missing config files
    There's no reason for us to have git-config(1) warn users when a
    config file is entirely missing.

--- lib/PublicInbox/Config.pm
+++ lib/PublicInbox/Config.pm
@@ -147,6 +147,7 @@ sub default_file {
 sub git_config_dump {
 	my ($file) = @_;
 	my (%section_seen, @section_order);
+	return {} unless -e $file;
 	my @cmd = (qw/git config/, "--file=$file", '-l');
 	my $cmd = join(' ', @cmd);
 	my $fh = popen_rd(\@cmd) or die "popen_rd failed for $file: $!\n";
