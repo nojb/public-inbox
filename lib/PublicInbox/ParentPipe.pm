7344d5bb292a29b52b574dfb09542e3290430afd
Eric Wong <e@80x24.org>
2019-06-28 18:46:10 +0000

    parentpipe: document and use one-shot wakeups
    The master process only dies once and we close ourselves right
    away.  So it doesn't matter if it's level-triggered or
    edge-triggered, actually, but one-shot is most consistent with
    our use and keeps the kernel from doing extra work.

--- lib/PublicInbox/ParentPipe.pm
+++ lib/PublicInbox/ParentPipe.pm
@@ -1,20 +1,24 @@
 # Copyright (C) 2016-2018 all contributors <meta@public-inbox.org>
 # License: AGPL-3.0+ <https://www.gnu.org/licenses/agpl-3.0.txt>
-# only for PublicInbox::Daemon
+
+# only for PublicInbox::Daemon, allows worker processes to be
+# notified if the master process dies.
 package PublicInbox::ParentPipe;
 use strict;
 use warnings;
 use base qw(PublicInbox::DS);
 use fields qw(cb);
+use PublicInbox::Syscall qw(EPOLLIN EPOLLONESHOT);
 
 sub new ($$$) {
-	my ($class, $pipe, $cb) = @_;
+	my ($class, $pipe, $worker_quit) = @_;
 	my $self = fields::new($class);
-	$self->SUPER::new($pipe, PublicInbox::DS::EPOLLIN());
-	$self->{cb} = $cb;
+	$self->SUPER::new($pipe, EPOLLIN|EPOLLONESHOT);
+	$self->{cb} = $worker_quit;
 	$self;
 }
 
+# master process died, time to call worker_quit ourselves
 sub event_step { $_[0]->{cb}->($_[0]) }
 
 1;
