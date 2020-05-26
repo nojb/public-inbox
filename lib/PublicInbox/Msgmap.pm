nojb/public-inbox

commit 78888d36fb80889f6b0f8ca6f26edda145c14238

msgmap: use TRUNCATE for journal_mode, for now
It avoids I/O on the directory itself, which could prolong
the lifetime of the storage device.

lib/PublicInbox/Msgmap.pm
@@ -48,6 +48,10 @@ sub new_file {
 
 	if ($writable) {
 		create_tables($dbh);
+
+		# TRUNCATE reduces I/O compared to the default (DELETE)
+		$dbh->do('PRAGMA journal_mode = TRUNCATE');
+
 		$dbh->begin_work;
 		$self->created_at(time) unless $self->created_at;
 

commit 6fb0ce0b872baa1d2d7cb7e37e6282b2cd58feb6

msgmap: tmp_clone: use in-memory journal
This prevents $TMPDIR from being littered with *-journal files
after running the test suite.

This shouldn't cause excessive memory use since $v2w->{mm_tmp}
doesn't see big transactions.  There's no need to worry about
data loss, here,either, since this is just a temporary clone
we've even disabled fsync on.

Fixes: 78888d36fb80889f ("msgmap: use TRUNCATE for journal_mode, for now")

lib/PublicInbox/Msgmap.pm
@@ -70,6 +70,7 @@ sub tmp_clone {
 	$self->{dbh}->sqlite_backup_to_file($fn);
 	my $tmp = ref($self)->new_file($fn, 1);
 	$tmp->{dbh}->do('PRAGMA synchronous = OFF');
+	$tmp->{dbh}->do('PRAGMA journal_mode = MEMORY');
 	$tmp->{tmp_name} = $fn; # SQLite won't work if unlinked, apparently
 	$tmp->{pid} = $$;
 	close $fh or die "failed to close $fn: $!";