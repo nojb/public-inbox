nojb/public-inbox

commit 74a3206babe0572a1494500d21267a31873af7b0

mda: support multiple List-ID matches
While it's not RFC2919-conformant, mail software can
theoretically set multiple List-ID headers.  Deliver to all
inboxes which match a given List-ID since that's likely the
intended.

Cc: Eric W. Biederman <ebiederm@xmission.com>
Link: https://public-inbox.org/meta/87pniltscf.fsf@x220.int.ebiederm.org/

script/public-inbox-learn
@@ -95,8 +95,9 @@ if ($train eq 'spam') {
 		next if $seen{"$ibx"}++;
 		remove_or_add($ibx, $train, $addr);
 	}
-	my $ibx = PublicInbox::MDA->inbox_for_list_id($pi_config, $mime);
-	if ($ibx && !$seen{"$ibx"}) {
+	my $dests = PublicInbox::MDA->inboxes_for_list_id($pi_config, $mime);
+	for my $ibx (@$dests) {
+		next if !$seen{"$ibx"}++;
 		remove_or_add($ibx, $train, $ibx->{-primary_address});
 	}
 }

t/mda.t
@@ -308,6 +308,25 @@ EOF
 	my $cur = `git --git-dir=$maindir diff HEAD~1..HEAD`;
 	like($cur, qr/this message would not be accepted without --no-precheck/,
 		'--no-precheck delivered message anyways');
+
+	# try a message with multiple List-ID headers
+	$in = <<EOF;
+List-ID: <foo.bar>
+List-ID: <$list_id>
+Message-ID: <2lids\@example>
+Subject: two List-IDs
+From: user <user\@example.com>
+To: $addr
+Date: Fri, 02 Oct 1993 00:00:00 +0000
+
+EOF
+	($out, $err) = ('', '');
+	IPC::Run::run([$mda], \$in, \$out, \$err);
+	is($?, 0, 'mda OK with multiple List-Id matches');
+	$cur = `git --git-dir=$maindir diff HEAD~1..HEAD`;
+	like($cur, qr/Message-ID: <2lids\@example>/,
+		'multi List-ID match delivered');
+	like($err, qr/multiple List-ID/, 'warned about multiple List-ID');
 }
 
 done_testing();

lib/PublicInbox/MDA.pm
@@ -83,19 +83,25 @@ sub set_list_headers {
 	}
 }
 
-# TODO: deal with multiple List-ID headers?
-sub inbox_for_list_id ($$) {
+sub inboxes_for_list_id ($$) {
 	my ($klass, $config, $simple) = @_;
 
 	# newer Email::Simple allows header_raw, as does Email::MIME:
-	my $list_id = $simple->can('header_raw') ?
+	my @list_ids = $simple->can('header_raw') ?
 			$simple->header_raw('List-Id') :
 			$simple->header('List-Id');
-	my $ibx;
-	if (defined $list_id && $list_id =~ /<[ \t]*(.+)?[ \t]*>/) {
-		$ibx = $config->lookup_list_id($1);
+	my @dests;
+	for my $list_id (@list_ids) {
+		$list_id =~ /<[ \t]*(.+)?[ \t]*>/ or next;
+		if (my $ibx = $config->lookup_list_id($1)) {
+			push @dests, $ibx;
+		}
 	}
-	$ibx;
+	if (scalar(@list_ids) > 1) {
+		warn "W: multiple List-IDs in message:\n";
+		warn "W: List-ID: $_\n" for @list_ids
+	}
+	\@dests;
 }
 
 1;

script/public-inbox-mda
@@ -44,12 +44,11 @@ if (defined $recipient) {
 	push @$dests, $ibx if $ibx;
 }
 if (!scalar(@$dests)) {
-	my $ibx = PublicInbox::MDA->inbox_for_list_id($config, $simple);
-	if (!defined($ibx) && !defined($recipient)) {
+	$dests = PublicInbox::MDA->inboxes_for_list_id($config, $simple);
+	if (!scalar(@$dests) && !defined($recipient)) {
 		die "ORIGINAL_RECIPIENT not defined in ENV\n";
 	}
-	defined($ibx) or do_exit(67); # EX_NOUSER 5.1.1 user unknown
-	push @$dests, $ibx;
+	scalar(@$dests) or do_exit(67); # EX_NOUSER 5.1.1 user unknown
 }
 
 my $err;

commit e97f478cd6ace58cbbe6287106f45f7809f5e8b1

learn: fix buggy typo on List-ID mapping
There is obviously a typo here, so fix it and add a test
case to guard against future regressions.

Fixes: 74a3206babe0572a ("mda: support multiple List-ID matches")

script/public-inbox-learn
@@ -97,7 +97,7 @@ if ($train eq 'spam') {
 	}
 	my $dests = PublicInbox::MDA->inboxes_for_list_id($pi_config, $mime);
 	for my $ibx (@$dests) {
-		next if !$seen{"$ibx"}++;
+		next if $seen{"$ibx"}++;
 		remove_or_add($ibx, $train, $mime, $ibx->{-primary_address});
 	}
 }

t/mda.t
@@ -299,9 +299,17 @@ EOF
 	ok(run_script(['-mda'], undef, $rdr),
 		'mda OK with multiple List-Id matches');
 	$cur = $git->qx(qw(diff HEAD~1..HEAD));
-	like($cur, qr/Message-ID: <2lids\@example>/,
+	like($cur, qr/^\+Message-ID: <2lids\@example>/sm,
 		'multi List-ID match delivered');
 	like($err, qr/multiple List-ID/, 'warned about multiple List-ID');
+
+	# ensure -learn rm works after inbox address is updated
+	($out, $err) = ('', '');
+	xsys(qw(git config --file), $pi_config, "$cfgpfx.address",
+		'updated-address@example.com');
+	ok(run_script(['-learn', 'rm'], undef, $rdr), 'rm-ed via -learn');
+	$cur = $git->qx(qw(diff HEAD~1..HEAD));
+	like($cur, qr/^-Message-ID: <2lids\@example>/sm, 'changed in git');
 }
 
 done_testing();