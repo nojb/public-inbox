nojb/public-inbox

commit b32b47fb12a3043d748f723802de923d9043d7ff

index: "git log" failures are fatal
While I've never seen "git log" fail on its own, it could happen
one day and we should be prepared to abort indexing when it
happens.

Beef up tests for t/spawn.t to ensure close() behaves
on popen_rd the way we expect it to.

lib/PublicInbox/SearchIdx.pm
@@ -590,6 +590,7 @@ sub read_log {
 			$newest ||= $latest;
 		}
 	}
+	close($log) or die "git log failed: \$?=$?";
 	# get the leftovers
 	foreach my $blob (keys %D) {
 		my $mime = do_cat_mail($git, $blob, \$bytes) or next;
@@ -632,7 +633,7 @@ sub _git_log {
 			     --no-notes --no-color --no-renames
 			     --diff-filter=AM), $range);
 	++$fcount while <$fh>;
-	close $fh;
+	close $fh or die "git log failed: \$?=$?";
 	my $high = $self->{mm}->num_highwater;
 	$pr->("$fcount\n") if $pr; # continue previous line
 	$self->{ntodo} = $fcount;
@@ -713,7 +714,10 @@ sub _index_sync {
 	my $xdb = $self->begin_txn_lazy;
 	my $mm = _msgmap_init($self);
 	do {
-		$xlog = undef;
+		if ($xlog) {
+			close($xlog) or die "git log failed: \$?=$?";
+			$xlog = undef;
+		}
 		$last_commit = _last_x_commit($self, $mm);
 		$lx = reindex_from($opts->{reindex}, $last_commit);
 

lib/PublicInbox/V2Writable.pm
@@ -1128,6 +1128,7 @@ sub sync_prepare ($$$) {
 				--no-notes --no-color --no-renames
 				--diff-filter=AM), $range, '--', 'm');
 		++$n while <$fh>;
+		close $fh or die "git log failed: \$?=$?";
 		$pr->("$n\n") if $pr;
 		$regen_max += $n;
 	}
@@ -1195,7 +1196,7 @@ sub unindex ($$$$) {
 		unindex_oid($self, $git, $1, $unindexed);
 	}
 	delete $self->{reindex_pipe};
-	$fh = undef;
+	close $fh or die "git log failed: \$?=$?";
 
 	return unless $sync->{-opt}->{prune};
 	my $after = scalar keys %$unindexed;
@@ -1251,7 +1252,7 @@ sub index_epoch ($$$) {
 			mark_deleted($self, $sync, $git, $1);
 		}
 	}
-	$fh = undef;
+	close $fh or die "git log failed: \$?=$?";
 	delete $self->{reindex_pipe};
 	update_last_commit($self, $git, $i, $cmt) if defined $cmt;
 }

t/spawn.t
@@ -71,13 +71,13 @@ use PublicInbox::Spawn qw(which spawn popen_rd);
 	is($buf, "hello\n", 'tied gets works');
 	is(sysread($fh, $buf, 6), 0, 'sysread got EOF');
 	$? = 1;
-	close $fh;
+	ok(close($fh), 'close succeeds');
 	is($?, 0, '$? set properly');
 }
 
 {
 	my $fh = popen_rd([qw(false)]);
-	close $fh;
+	ok(!close($fh), 'close fails on false');
 	isnt($?, 0, '$? set properly: '.$?);
 }
 

commit 5df08b6cd4931e87c1cf676014256d00e3cdfc2c

searchidx: v1: fix retries when Xapian and Msgmap are out-of-sync
We forcibly stop git-log here, so erroring out on git-log close
failures is wrong since it sees SIGPIPE.  Noticed while
reindexing a large v1 inbox for IMAP changes.

Fixes: b32b47fb12a3043d ("index: "git log" failures are fatal")

lib/PublicInbox/SearchIdx.pm
@@ -752,10 +752,7 @@ sub _index_sync {
 	my $xdb = $self->begin_txn_lazy;
 	my $mm = _msgmap_init($self);
 	do {
-		if ($xlog) {
-			close($xlog) or die "git log failed: \$?=$?";
-			$xlog = undef;
-		}
+		$xlog = undef; # stop previous git-log via SIGPIPE
 		$last_commit = _last_x_commit($self, $mm);
 		$lx = reindex_from($opts->{reindex}, $last_commit);
 