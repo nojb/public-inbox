bd0cddda0121bc80247b00a0797453fcdeba7e4c
Eric Wong <e@yhbt.net>
2020-07-05 23:27:31 +0000

    mboxgz: reduce hash depth
    We can bless $ctx directly into a MboxGz object to reduce
    hash lookups and allocations.

--- lib/PublicInbox/MboxGz.pm
+++ lib/PublicInbox/MboxGz.pm
@@ -10,20 +10,10 @@ use PublicInbox::GitAsyncCat;
 *msg_hdr = \&PublicInbox::Mbox::msg_hdr;
 *msg_body = \&PublicInbox::Mbox::msg_body;
 
-sub new {
-	my ($class, $ctx, $cb) = @_;
-	$ctx->{base_url} = $ctx->{-inbox}->base_url($ctx->{env});
-	bless {
-		gz => PublicInbox::GzipFilter::gzip_or_die(),
-		cb => $cb,
-		ctx => $ctx
-	}, $class;
-}
-
 # this is public-inbox-httpd-specific
 sub mboxgz_blob_cb { # git->cat_async callback
 	my ($bref, $oid, $type, $size, $self) = @_;
-	my $http = $self->{ctx}->{env}->{'psgix.io'} or return; # client abort
+	my $http = $self->{env}->{'psgix.io'} or return; # client abort
 	my $smsg = delete $self->{smsg} or die 'BUG: no smsg';
 	if (!defined($oid)) {
 		# it's possible to have TOCTOU if an admin runs
@@ -32,7 +22,7 @@ sub mboxgz_blob_cb { # git->cat_async callback
 	} else {
 		$smsg->{blob} eq $oid or die "BUG: $smsg->{blob} != $oid";
 	}
-	$self->zmore(msg_hdr($self->{ctx},
+	$self->zmore(msg_hdr($self,
 				PublicInbox::Eml->new($bref)->header_obj,
 				$smsg->{mid}));
 
@@ -45,8 +35,8 @@ sub mboxgz_blob_cb { # git->cat_async callback
 # this is public-inbox-httpd-specific
 sub async_step ($) {
 	my ($self) = @_;
-	if (my $smsg = $self->{smsg} = $self->{cb}->($self->{ctx})) {
-		git_async_cat($self->{ctx}->{-inbox}->git, $smsg->{blob},
+	if (my $smsg = $self->{smsg} = $self->{cb}->($self)) {
+		git_async_cat($self->{-inbox}->git, $smsg->{blob},
 				\&mboxgz_blob_cb, $self);
 	} elsif (my $out = delete $self->{http_out}) {
 		$out->write($self->zflush);
@@ -64,17 +54,20 @@ sub async_next {
 sub close { !!delete($_[0]->{http_out}) }
 
 sub response {
-	my ($class, $ctx, $cb, $fn) = @_;
-	my $self = $class->new($ctx, $cb);
+	my ($class, $self, $cb, $fn) = @_;
+	$self->{base_url} = $self->{-inbox}->base_url($self->{env});
+	$self->{cb} = $cb;
+	$self->{gz} = PublicInbox::GzipFilter::gzip_or_die();
+	bless $self, $class;
 	# http://www.iana.org/assignments/media-types/application/gzip
 	$fn = defined($fn) && $fn ne '' ? to_filename($fn) : 'no-subject';
 	my $h = [ qw(Content-Type application/gzip),
 		'Content-Disposition', "inline; filename=$fn.mbox.gz" ];
-	if ($ctx->{env}->{'pi-httpd.async'}) {
+	if ($self->{env}->{'pi-httpd.async'}) {
 		sub {
 			my ($wcb) = @_; # -httpd provided write callback
 			$self->{http_out} = $wcb->([200, $h]);
-			$self->{ctx}->{env}->{'psgix.io'}->{forward} = $self;
+			$self->{env}->{'psgix.io'}->{forward} = $self;
 			async_step($self); # start stepping
 		};
 	} else { # generic PSGI
@@ -85,15 +78,15 @@ sub response {
 # called by Plack::Util::foreach or similar (generic PSGI)
 sub getline {
 	my ($self) = @_;
-	my $ctx = $self->{ctx} or return;
-	while (my $smsg = $self->{cb}->($ctx)) {
-		my $mref = $ctx->{-inbox}->msg_by_smsg($smsg) or next;
+	my $cb = $self->{cb} or return;
+	while (my $smsg = $cb->($self)) {
+		my $mref = $self->{-inbox}->msg_by_smsg($smsg) or next;
 		my $h = PublicInbox::Eml->new($mref)->header_obj;
-		$self->zmore(msg_hdr($ctx, $h, $smsg->{mid}));
+		$self->zmore(msg_hdr($self, $h, $smsg->{mid}));
 		return $self->translate(msg_body($$mref));
 	}
 	# signal that we're done and can return undef next call:
-	delete $self->{ctx};
+	delete $self->{cb};
 	$self->zflush;
 }
 
