nojb/public-inbox

commit a318e758129d616b3b801db82c4735c680a2fbe8

make zlib-related modules a hard dependency
This allows us to simplify some of our existing code and make
future changes easier.

I doubt anybody goes through the trouble to have a Perl
installation without zlib support.  The zlib source code is even
bundled with Perl since 5.9.3 for systems without existing zlib
development headers and libraries.

Of course, zlib is also a requirement of git, too; and we're not
going to stop using git :)

[squashed: "wwwaltid: use gzipfilter up front"]

lib/PublicInbox/Mbox.pm
@@ -152,8 +152,7 @@ sub thread_cb {
 
 sub thread_mbox {
 	my ($ctx, $over, $sfx) = @_;
-	eval { require PublicInbox::MboxGz };
-	return need_gzip($ctx) if $@;
+	require PublicInbox::MboxGz;
 	my $msgs = $ctx->{msgs} = $over->get_thread($ctx->{mid}, {});
 	return [404, [qw(Content-Type text/plain)], []] if !@$msgs;
 	$ctx->{prev} = $msgs->[-1];
@@ -221,8 +220,7 @@ sub results_cb {
 sub mbox_all {
 	my ($ctx, $query) = @_;
 
-	eval { require PublicInbox::MboxGz };
-	return need_gzip($ctx) if $@;
+	require PublicInbox::MboxGz;
 	return mbox_all_ids($ctx) if $query eq '';
 	my $qopts = $ctx->{qopts} = { mset => 2 };
 	my $srch = $ctx->{srch} = $ctx->{-inbox}->search or
@@ -234,17 +232,6 @@ sub mbox_all {
 	$ctx->{iter} = 0;
 	$ctx->{query} = $query;
 	PublicInbox::MboxGz->response($ctx, \&results_cb, 'results-'.$query);
-}
-
-sub need_gzip {
-	html_oneshot($_[0], 501, \<<EOF);
-<pre>gzipped mbox not available
-
-The administrator needs to install the Compress::Raw::Zlib Perl module
-to support gzipped mboxes.
-
-<a href="../">Return to index</a></pre>
-EOF
 }
 
 1;

Makefile.PL
@@ -132,6 +132,11 @@ WriteMakefile(
 		# `perl5' on FreeBSD
 		'Encode' => 0,
 
+		# libperl$PERL_VERSION + perl-modules-$PERL_VERSION
+		'Compress::Raw::Zlib' => 0,
+		'Compress::Zlib' => 0,
+		'IO::Compress::Gzip' => 0,
+
 		# Plack is needed for public-inbox-httpd and PublicInbox::WWW
 		# 'Plack' => 0,
 

ci/deps.perl
@@ -25,6 +25,7 @@ my $profiles = {
 		Email::MIME::ContentType
 		Encode
 		ExtUtils::MakeMaker
+		IO::Compress::Gzip
 		URI::Escape
 		), @test_essential ],
 
@@ -35,7 +36,6 @@ my $profiles = {
 		DBD::SQLite
 		DBI
 		Filesys::Notify::Simple
-		IO::Compress::Gzip
 		Inline::C
 		Net::Server
 		Plack

lib/PublicInbox/NNTP.pm
@@ -38,8 +38,7 @@ NEWNEWS\r
 LIST ACTIVE ACTIVE.TIMES NEWSGROUPS OVERVIEW.FMT\r
 HDR\r
 OVER\r
-
-my $have_deflate;
+COMPRESS DEFLATE\r
 
 sub greet ($) { $_[0]->write($_[0]->{nntpd}->{greet}) };
 
@@ -903,7 +902,7 @@ sub cmd_starttls ($) {
 sub cmd_compress ($$) {
 	my ($self, $alg) = @_;
 	return '503 Only DEFLATE is supported' if uc($alg) ne 'DEFLATE';
-	return r502 if $self->compressed || !$have_deflate;
+	return r502 if $self->compressed;
 	PublicInbox::NNTPdeflate->enable($self);
 	$self->requeue;
 	undef
@@ -988,12 +987,6 @@ sub event_step {
 sub busy {
 	my ($self, $now) = @_;
 	($self->{rbuf} || $self->{wbuf} || $self->not_idle_long($now));
-}
-
-# this is an import to prevent "perl -c" from complaining about fields
-sub import {
-	$have_deflate = eval { require PublicInbox::NNTPdeflate } and
-		$CAPABILITIES .= "COMPRESS DEFLATE\r\n";
 }
 
 1;

lib/PublicInbox/WwwAltId.pm
@@ -8,6 +8,7 @@ use PublicInbox::Qspawn;
 use PublicInbox::WwwStream qw(html_oneshot);
 use PublicInbox::AltId;
 use PublicInbox::Spawn qw(which);
+use PublicInbox::GzipFilter;
 our $sqlite3 = $ENV{SQLITE3};
 
 sub sqlite3_missing ($) {
@@ -64,13 +65,6 @@ or
 EOF
 	}
 
-	eval { require PublicInbox::GzipFilter } or
-		return html_oneshot($ctx, 501, \<<EOF);
-<pre>gzip output not available
-
-The administrator needs to install the Compress::Raw::Zlib Perl module
-to support gzipped sqlite3 dumps.</pre>
-EOF
 	$sqlite3 //= which('sqlite3');
 	if (!defined($sqlite3)) {
 		return html_oneshot($ctx, 501, \<<EOF);

lib/PublicInbox/WwwListing.pm
@@ -14,14 +14,13 @@ use bytes (); # bytes::length
 use HTTP::Date qw(time2str);
 use Digest::SHA ();
 use File::Spec ();
+use IO::Compress::Gzip qw(gzip);
 *try_cat = \&PublicInbox::Inbox::try_cat;
 our $json;
-if (eval { require IO::Compress::Gzip }) {
-	for my $mod (qw(JSON::MaybeXS JSON JSON::PP)) {
-		eval "require $mod" or next;
-		# ->ascii encodes non-ASCII to "\uXXXX"
-		$json = $mod->new->ascii(1) and last;
-	}
+for my $mod (qw(JSON::MaybeXS JSON JSON::PP)) {
+	eval "require $mod" or next;
+	# ->ascii encodes non-ASCII to "\uXXXX"
+	$json = $mod->new->ascii(1) and last;
 }
 
 sub list_all_i {
@@ -220,7 +219,7 @@ sub js ($$) {
 		$repo->{reference} = $abs2urlpath->{$abs};
 	}
 	my $out;
-	IO::Compress::Gzip::gzip(\($json->encode($manifest)) => \$out);
+	gzip(\($json->encode($manifest)) => \$out);
 	$manifest = undef;
 	[ 200, [ qw(Content-Type application/gzip),
 		 'Last-Modified', time2str($mtime),

script/public-inbox-nntpd
@@ -6,7 +6,7 @@
 use strict;
 use warnings;
 use PublicInbox::Daemon;
-use PublicInbox::NNTP; # need to call import
+use PublicInbox::NNTPdeflate; # loads PublicInbox::NNTP
 use PublicInbox::NNTPD;
 my $nntpd = PublicInbox::NNTPD->new;
 PublicInbox::Daemon::run('0.0.0.0:119',

commit c2ae55ff47069952ac181111e83e550d7ff3c710

mbox: remove html_oneshot import
It's no longer needed, we no longer show a runtime error
for zlib being missing, as zlib is a hard requirement.

Fixes: a318e758129d616b ("make zlib-related modules a hard dependency")

lib/PublicInbox/Mbox.pm
@@ -13,7 +13,6 @@ use warnings;
 use PublicInbox::MID qw/mid_escape/;
 use PublicInbox::Hval qw/to_filename/;
 use PublicInbox::Smsg;
-use PublicInbox::WwwStream qw(html_oneshot);
 use PublicInbox::Eml;
 
 sub subject_fn ($) {