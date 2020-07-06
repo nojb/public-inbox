nojb/public-inbox

commit 10ee3548084c125f20fe2c830faea2a43413be92

git_async_cat: remove circular reference
While this circular reference was carefully managed to not leak
memory; it was still triggering a warning at -imapd/-nntpd
shutdown due to the EPOLL_CTL_DEL op failing after the $Epoll FD
gets closed.

So remove the circular reference by providing a ref to `undef',
instead.

lib/PublicInbox/GitAsyncCat.pm
@@ -15,20 +15,20 @@ use fields qw(git);
 use PublicInbox::Syscall qw(EPOLLIN EPOLLET);
 our @EXPORT = qw(git_async_cat);
 
-sub new {
+sub _add {
 	my ($class, $git) = @_;
 	my $self = fields::new($class);
 	$git->batch_prepare;
 	$self->SUPER::new($git->{in}, EPOLLIN|EPOLLET);
 	$self->{git} = $git;
-	$self;
+	\undef; # this is a true ref()
 }
 
 sub event_step {
 	my ($self) = @_;
 	my $git = $self->{git} or return; # ->close-ed
 	my $inflight = $git->{inflight};
-	if (@$inflight) {
+	if ($inflight && @$inflight) {
 		$git->cat_async_step($inflight);
 		$self->requeue if @$inflight || exists $git->{cat_rbuf};
 	}
@@ -37,7 +37,7 @@ sub event_step {
 sub close {
 	my ($self) = @_;
 	if (my $git = delete $self->{git}) {
-		delete $git->{async_cat}; # drop circular reference
+		delete $git->{async_cat};
 	}
 	$self->SUPER::close; # PublicInbox::DS::close
 }
@@ -45,7 +45,7 @@ sub close {
 sub git_async_cat ($$$$) {
 	my ($git, $oid, $cb, $arg) = @_;
 	$git->cat_async($oid, $cb, $arg);
-	$git->{async_cat} //= new(__PACKAGE__, $git); # circular reference
+	$git->{async_cat} //= _add(__PACKAGE__, $git);
 }
 
 1;

lib/PublicInbox/Git.pm
@@ -295,9 +295,7 @@ sub qx {
 sub cleanup {
 	my ($self) = @_;
 	local $in_cleanup = 1;
-	if (my $ac = $self->{async_cat}) {
-		$ac->close; # PublicInbox::GitAsyncCat::close -> EPOLL_CTL_DEL
-	}
+	delete $self->{async_cat};
 	cat_async_wait($self);
 	_destroy($self, qw(cat_rbuf in out pid));
 	_destroy($self, qw(chk_rbuf in_c out_c pid_c err_c));

lib/PublicInbox/IMAP.pm
@@ -1294,7 +1294,7 @@ sub long_step {
 	} elsif ($more) { # $self->{wbuf}:
 		$self->update_idle_time;
 
-		# control passed to $more may be a GitAsyncCat object
+		# control passed to git_async_cat if $more == \undef
 		requeue_once($self) if !ref($more);
 	} else { # all done!
 		delete $self->{long_cb};

lib/PublicInbox/NNTP.pm
@@ -553,8 +553,7 @@ sub cmd_article ($;$) {
 	return $smsg unless ref $smsg;
 	set_art($self, $art);
 	$smsg->{nntp} = $self;
-	git_async_cat($self->{ng}->git, $smsg->{blob}, \&blob_cb, $smsg);
-	undef;
+	${git_async_cat($self->{ng}->git, $smsg->{blob}, \&blob_cb, $smsg)};
 }
 
 sub cmd_head ($;$) {
@@ -564,8 +563,7 @@ sub cmd_head ($;$) {
 	set_art($self, $art);
 	$smsg->{nntp} = $self;
 	$smsg->{nntp_code} = 221;
-	git_async_cat($self->{ng}->git, $smsg->{blob}, \&blob_cb, $smsg);
-	undef;
+	${git_async_cat($self->{ng}->git, $smsg->{blob}, \&blob_cb, $smsg)};
 }
 
 sub cmd_body ($;$) {
@@ -575,8 +573,7 @@ sub cmd_body ($;$) {
 	set_art($self, $art);
 	$smsg->{nntp} = $self;
 	$smsg->{nntp_code} = 222;
-	git_async_cat($self->{ng}->git, $smsg->{blob}, \&blob_cb, $smsg);
-	undef;
+	${git_async_cat($self->{ng}->git, $smsg->{blob}, \&blob_cb, $smsg)};
 }
 
 sub cmd_stat ($;$) {

commit 2c6e8b10e3f9f3b047009126426ce634c8f29a23

git_async_cat: unref pipes on EOF from git->cleanup
We avoided a managed circular reference in 10ee3548084c125f
but introduced a pipe FD leak, instead.  So handle the EOF
we get when the "git cat-file --batch" process exits and
closes its stdout FD.

v2: remove ->close entirely.  PublicInbox::Git->cleanup
handles all cleanup.  This prevents us from inadvertantly
deleting the {async_cat} field associated with a different
pipe than the one GAC is monitoring.

Fixes: 10ee3548084c125f ("git_async_cat: remove circular reference")

lib/PublicInbox/GitAsyncCat.pm
@@ -24,20 +24,13 @@ sub _add {
 
 sub event_step {
 	my ($self) = @_;
-	my $git = $self->{git} or return; # ->close-ed
+	my $git = $self->{git};
+	return $self->close if ($git->{in} // 0) != ($self->{sock} // 1);
 	my $inflight = $git->{inflight};
 	if ($inflight && @$inflight) {
 		$git->cat_async_step($inflight);
 		$self->requeue if @$inflight || exists $git->{cat_rbuf};
 	}
-}
-
-sub close {
-	my ($self) = @_;
-	if (my $git = delete $self->{git}) {
-		delete $git->{async_cat};
-	}
-	$self->SUPER::close; # PublicInbox::DS::close
 }
 
 sub git_async_cat ($$$$) {