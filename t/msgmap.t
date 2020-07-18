nojb/public-inbox

commit f06e84220e5566e74c4af675a7afaf1636b024e4

over+msgmap: do not store filename after DBI->connect
SQLite already knows the filename internally, so avoid having it
as a long-lived Perl SV to save some bytes when there's many
inboxes and open DBs.

lib/PublicInbox/Msgmap.pm
@@ -229,9 +229,9 @@ sub mid_set {
 
 sub DESTROY {
 	my ($self) = @_;
-	delete $self->{dbh};
-	my $f = $self->{filename};
+	my $dbh = $self->{dbh} or return;
 	if (($self->{pid} // 0) == $$) {
+		my $f = $dbh->sqlite_db_filename;
 		unlink $f or warn "failed to unlink $f: $!\n";
 	}
 }
@@ -239,8 +239,9 @@ sub DESTROY {
 sub atfork_parent {
 	my ($self) = @_;
 	$self->{pid} or die "not a temporary clone\n";
-	delete $self->{dbh} and die "tmp_clone dbh not prepared for parent";
-	my $dbh = $self->{dbh} = PublicInbox::Over::dbh_new($self, 1);
+	my $dbh = $self->{dbh} and die "tmp_clone dbh not prepared for parent";
+	$self->{filename} = $dbh->sqlite_db_filename;
+	$dbh = $self->{dbh} = PublicInbox::Over::dbh_new($self, 1);
 	$dbh->do('PRAGMA synchronous = OFF');
 }
 
@@ -249,9 +250,10 @@ sub atfork_prepare {
 	$self->{pid} or die "not a temporary clone\n";
 	$self->{pid} == $$ or
 		die "BUG: atfork_prepare not called from $self->{pid}\n";
-	$self->{dbh} or die "temporary clone not open\n";
+	my $dbh = $self->{dbh} or die "temporary clone not open\n";
+
 	# must clobber prepared statements
-	%$self = (filename => $self->{filename}, pid => $$);
+	%$self = (filename => $dbh->sqlite_db_filename, pid => $$);
 }
 
 sub skip_artnum {

lib/PublicInbox/Inbox.pm
@@ -204,9 +204,9 @@ sub search ($;$) {
 sub over ($) {
 	my ($self) = @_;
 	my $srch = search($self, 1) or return;
-	$self->{over} ||= eval {
+	$self->{over} //= eval {
 		my $over = $srch->{over_ro};
-		$over->dbh_new; # may fail
+		$over->connect; # may fail
 		$over;
 	}
 }

lib/PublicInbox/Over.pm
@@ -15,9 +15,13 @@ use constant DEFAULT_LIMIT => 1000;
 
 sub dbh_new {
 	my ($self, $rw) = @_;
-	my $f = $self->{filename};
-	if ($rw && !-f $f) { # SQLite defaults mode to 0644, we want 0666
-		open my $fh, '+>>', $f or die "failed to open $f: $!";
+	my $f = delete $self->{filename};
+	if (!-f $f) { # SQLite defaults mode to 0644, we want 0666
+		if ($rw) {
+			open my $fh, '+>>', $f or die "failed to open $f: $!";
+		} else {
+			$self->{filename} = $f; # die on stat() below:
+		}
 	}
 	my (@st, $st, $dbh);
 	my $tries = 0;
@@ -44,9 +48,14 @@ sub new {
 	bless { filename => $f }, $class;
 }
 
-sub disconnect { $_[0]->{dbh} = undef }
+sub disconnect {
+	my ($self) = @_;
+	if (my $dbh = delete $self->{dbh}) {
+		$self->{filename} = $dbh->sqlite_db_filename;
+	}
+}
 
-sub connect { $_[0]->{dbh} ||= $_[0]->dbh_new }
+sub connect { $_[0]->{dbh} //= $_[0]->dbh_new }
 
 sub load_from_row ($;$) {
 	my ($smsg, $cull) = @_;
@@ -258,13 +267,18 @@ SELECT COUNT(num) FROM over WHERE num > ? AND num <= ?
 
 sub check_inodes {
 	my ($self) = @_;
-	if (my @st = stat($self->{filename})) { # did st_dev, st_ino change?
+	my $dbh = $self->{dbh} or return;
+	my $f = $dbh->sqlite_db_filename;
+	if (my @st = stat($f)) { # did st_dev, st_ino change?
 		my $st = pack('dd', $st[0], $st[1]);
 
 		# don't actually reopen, just let {dbh} be recreated later
-		delete($self->{dbh}) if ($st ne ($self->{st} // $st));
+		if ($st ne ($self->{st} // $st)) {
+			delete($self->{dbh});
+			$self->{filename} = $f;
+		}
 	} else {
-		warn "W: stat $self->{filename}: $!\n";
+		warn "W: stat $f: $!\n";
 	}
 }
 

lib/PublicInbox/OverIdx.pm
@@ -431,7 +431,7 @@ sub rollback_lazy {
 sub disconnect {
 	my ($self) = @_;
 	die "in transaction" if $self->{txn};
-	$self->{dbh} = undef;
+	$self->SUPER::disconnect;
 }
 
 sub create {

t/nntpd.t
@@ -343,7 +343,7 @@ Date: Fri, 02 Oct 1993 00:00:00 +0000
 		$im->add($ex);
 		$im->done;
 		{
-			my $f = $ibx->mm->{filename};
+			my $f = $ibx->mm->{dbh}->sqlite_db_filename;
 			my $tmp = "$tmpdir/tmp.sqlite3";
 			$ibx->mm->{dbh}->sqlite_backup_to_file($tmp);
 			delete $ibx->{mm};

commit 0ad076b1943bf7c65868d1db3cef4bd0ea1ef49a

msgmap: fix atfork_* callbacks
Noticed while reindexing a largish v2 inbox in parallel on an
SSD which required checkpointing and respawning shard workers.

Fixes: f06e84220e5566e7 ("over+msgmap: do not store filename after DBI->connect")

lib/PublicInbox/Msgmap.pm
@@ -238,22 +238,21 @@ sub DESTROY {
 
 sub atfork_parent {
 	my ($self) = @_;
-	$self->{pid} or die "not a temporary clone\n";
-	my $dbh = $self->{dbh} and die "tmp_clone dbh not prepared for parent";
-	$self->{filename} = $dbh->sqlite_db_filename;
-	$dbh = $self->{dbh} = PublicInbox::Over::dbh_new($self, 1);
+	$self->{pid} or die 'BUG: not a temporary clone';
+	$self->{dbh} and die 'BUG: tmp_clone dbh not prepared for parent';
+	defined($self->{filename}) or die 'BUG: {filename} not defined';
+	my $dbh = $self->{dbh} = PublicInbox::Over::dbh_new($self, 1);
 	$dbh->do('PRAGMA synchronous = OFF');
 }
 
 sub atfork_prepare {
 	my ($self) = @_;
-	$self->{pid} or die "not a temporary clone\n";
-	$self->{pid} == $$ or
-		die "BUG: atfork_prepare not called from $self->{pid}\n";
-	my $dbh = $self->{dbh} or die "temporary clone not open\n";
+	my $pid = $self->{pid} or die 'BUG: not a temporary clone';
+	$pid == $$ or die "BUG: atfork_prepare not called by $pid";
+	my $dbh = $self->{dbh} or die 'BUG: temporary clone not open';
 
 	# must clobber prepared statements
-	%$self = (filename => $dbh->sqlite_db_filename, pid => $$);
+	%$self = (filename => $dbh->sqlite_db_filename, pid => $pid);
 }
 
 sub skip_artnum {

t/msgmap.t
@@ -64,4 +64,10 @@ my $tmp = $d->tmp_clone;
 is_deeply([$d->minmax], [$tmp->minmax], 'Cloned temporary DB matches');
 ok($tmp->mid_delete('spam@2'), 'temporary DB is writable');
 
+is(eval {
+	$tmp->atfork_prepare;
+	$tmp->atfork_parent;
+	'ok'
+}, 'ok', 'atfork_* work on tmp_clone');
+
 done_testing();