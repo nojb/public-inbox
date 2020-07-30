nojb/public-inbox

commit 1988d730c0088e8b1f6193650c7ee929df8a2ed7

config: support multi-value inbox.*.*url
Since the beginning of this project, we've implicitly supported
inboxes with multiple URLs by relying on the Host: header sent
by the client ($env->{HTTP_HOST}).

We now offer the option to explicitly configure multiple URLs for
every inbox along with the ability to do a best-effort match for
matching hostnames.

lib/PublicInbox/WwwListing.pm
@@ -32,7 +32,7 @@ sub list_all ($$$) {
 sub list_match_domain_i {
 	my ($ibx, $arg) = @_;
 	my ($list, $hide_key, $re) = @$arg;
-	if (!$ibx->{-hide}->{$hide_key} && $ibx->{url} =~ $re) {
+	if (!$ibx->{-hide}->{$hide_key} && grep($re, @{$ibx->{url}})) {
 		push @$list, $ibx;
 	}
 }
@@ -90,7 +90,7 @@ sub ibx_entry {
   ${\$ibx->description}
 
 	if (defined(my $info_url = $ibx->{infourl})) {
-		$tmp .= '  ' . $info_url . "\n";
+		$tmp .= '  ' . PublicInbox::Hval::prurl($env, $info_url) . "\n";
 	}
 	$tmp;
 }

lib/PublicInbox/Config.pm
@@ -362,8 +362,8 @@ sub _fill {
 	my ($self, $pfx) = @_;
 	my $ibx = {};
 
-	foreach my $k (qw(inboxdir filter url newsgroup
-			infourl watch watchheader httpbackendmax
+	foreach my $k (qw(inboxdir filter newsgroup
+			watch watchheader httpbackendmax
 			replyto feedmax nntpserver indexlevel)) {
 		my $v = $self->{"$pfx.$k"};
 		$ibx->{$k} = $v if defined $v;
@@ -383,7 +383,8 @@ sub _fill {
 	}
 	# TODO: more arrays, we should support multi-value for
 	# more things to encourage decentralization
-	foreach my $k (qw(address altid nntpmirror coderepo hide listid)) {
+	foreach my $k (qw(address altid nntpmirror coderepo hide listid url
+			infourl)) {
 		if (defined(my $v = $self->{"$pfx.$k"})) {
 			$ibx->{$k} = _array($v);
 		}

lib/PublicInbox/Hval.pm
@@ -86,6 +86,12 @@ sub raw {
 
 sub prurl {
 	my ($env, $u) = @_;
+	if (ref($u) eq 'ARRAY') {
+		my $h = $env->{HTTP_HOST} // $env->{SERVER_NAME};
+		my @host_match = grep(/\b\Q$h\E\b/, @$u);
+		$u = $host_match[0] // $u->[0];
+		# fall through to below:
+	}
 	index($u, '//') == 0 ? "$env->{'psgi.url_scheme'}:$u" : $u;
 }
 

lib/PublicInbox/Inbox.pm
@@ -252,7 +252,7 @@ sub base_url {
 	} else {
 		# either called from a non-PSGI environment (e.g. NNTP/POP3)
 		$self->{-base_url} ||= do {
-			my $url = $self->{url} or return undef;
+			my $url = $self->{url}->[0] or return undef;
 			# expand protocol-relative URLs to HTTPS if we're
 			# not inside a web server
 			$url = "https:$url" if $url =~ m!\A//!;

t/config.t
@@ -27,7 +27,7 @@ my ($tmpdir, $for_destroy) = tmpdir();
 		'inboxdir' => '/home/pi/meta-main.git',
 		'address' => [ 'meta@public-inbox.org' ],
 		'domain' => 'public-inbox.org',
-		'url' => 'http://example.com/meta',
+		'url' => [ 'http://example.com/meta' ],
 		-primary_address => 'meta@public-inbox.org',
 		'name' => 'meta',
 		feedmax => 25,
@@ -48,7 +48,7 @@ my ($tmpdir, $for_destroy) = tmpdir();
 		'domain' => 'public-inbox.org',
 		'name' => 'test',
 		feedmax => 25,
-		'url' => 'http://example.com/test',
+		'url' => [ 'http://example.com/test' ],
 		-httpbackend_limiter => undef,
 		nntpserver => undef,
 	}, "lookup matches expected output for test");

t/feed.t
@@ -29,7 +29,7 @@ my $ibx = PublicInbox::Inbox->new({
 	address => 'test@example',
 	name => 'testbox',
 	inboxdir => $git_dir,
-	url => 'http://example.com/test',
+	url => [ 'http://example.com/test' ],
 	feedmax => 3,
 });
 my $git = $ibx->git;

t/inbox.t
@@ -4,9 +4,9 @@ use strict;
 use warnings;
 use Test::More;
 use_ok 'PublicInbox::Inbox';
-my $x = PublicInbox::Inbox->new({url => '//example.com/test/'});
+my $x = PublicInbox::Inbox->new({url => [ '//example.com/test/' ]});
 is($x->base_url, 'https://example.com/test/', 'expanded protocol-relative');
-$x = PublicInbox::Inbox->new({url => 'http://example.com/test'});
+$x = PublicInbox::Inbox->new({url => [ 'http://example.com/test' ]});
 is($x->base_url, 'http://example.com/test/', 'added trailing slash');
 
 $x = PublicInbox::Inbox->new({});

t/nntp.t
@@ -104,7 +104,7 @@ use_ok 'PublicInbox::Inbox';
 					-primary_address => 'a@example.com',
 					newsgroup => 'test',
 					domain => 'example.com',
-					url => '//example.com/a'});
+					url => [ '//example.com/a' ]});
 	is($ng->base_url, $u, 'URL expanded');
 	my $mid = 'a@b';
 	my $mime = Email::MIME->new("Message-ID: <$mid>\r\n\r\n");

commit 41ecd08ee60daa495de3a2fc2c0296dc9cc9a0b3

wwwlisting: fix grep call for match=domain filtering
The grep call in list_match_domain_i returns true for all inboxes,
even ones without a URL that matches the regular expression, because
the qr value passed to grep is not surrounded by slashes.  Add them.

Fixes: 1988d730c0088e8b (config: support multi-value inbox.*.*url)

lib/PublicInbox/WwwListing.pm
@@ -40,7 +40,7 @@ sub list_all ($$$) {
 sub list_match_domain_i {
 	my ($ibx, $arg) = @_;
 	my ($list, $hide_key, $re) = @$arg;
-	if (!$ibx->{-hide}->{$hide_key} && grep($re, @{$ibx->{url}})) {
+	if (!$ibx->{-hide}->{$hide_key} && grep(/$re/, @{$ibx->{url}})) {
 		push @$list, $ibx;
 	}
 }