nojb/public-inbox

commit a5968dc059f655a9ac62f1b8bf0bed2b90080b02

config: lazy-load coderepos, support extindex
Extsearch objects are duck-types of Inbox objects, and
are capable of supporting code repos all the same.

lib/PublicInbox/Config.pm
@@ -355,6 +355,12 @@ sub _fill_code_repo {
 	$git;
 }
 
+sub get_all {
+	my ($self, $key) = @_;
+	my $v = $self->{$key} // return;
+	_array($v);
+}
+
 sub git_bool {
 	my ($val) = $_[-1]; # $_[0] may be $self, or $val
 	if ($val =~ /\A(?:false|no|off|[\-\+]?(?:0x)?0+)\z/i) {
@@ -382,6 +388,34 @@ sub _one_val {
 	return $v if !ref($v);
 	warn "W: $pfx.$k has multiple values, only using `$v->[-1]'\n";
 	$v->[-1];
+}
+
+sub repo_objs {
+	my ($self, $ibxish) = @_;
+	my $ibx_code_repos = $ibxish->{coderepo} or return;
+	$ibxish->{-repo_objs} //= do {
+		my $code_repos = $self->{-code_repos};
+		my @repo_objs;
+		for my $nick (@$ibx_code_repos) {
+			my @parts = split(m!/!, $nick);
+			for (@parts) {
+				@parts = () unless valid_foo_name($_);
+			}
+			unless (@parts) {
+				warn "invalid coderepo name: `$nick'\n";
+				next;
+			}
+			my $repo = $code_repos->{$nick} //=
+						_fill_code_repo($self, $nick);
+			push @repo_objs, $repo if $repo;
+		}
+		if (scalar @repo_objs) {
+			\@repo_objs;
+		} else {
+			delete $ibxish->{coderepo};
+			undef;
+		}
+	}
 }
 
 sub _fill_ibx {
@@ -466,20 +500,6 @@ sub _fill_ibx {
 		$ibx->{-no_obfuscate} = $self->{-no_obfuscate};
 		$ibx->{-no_obfuscate_re} = $self->{-no_obfuscate_re};
 		fill_all($self); # noop to populate -no_obfuscate
-	}
-	if (my $ibx_code_repos = $ibx->{coderepo}) {
-		my $code_repos = $self->{-code_repos};
-		my $repo_objs = $ibx->{-repo_objs} = [];
-		foreach my $nick (@$ibx_code_repos) {
-			my @parts = split(m!/!, $nick);
-			my $valid = 0;
-			$valid += valid_foo_name($_) foreach (@parts);
-			$valid == scalar(@parts) or next;
-
-			my $repo = $code_repos->{$nick} //=
-						_fill_code_repo($self, $nick);
-			push @$repo_objs, $repo if $repo;
-		}
 	}
 	if (my $es = ALL($self)) {
 		require PublicInbox::Isearch;

lib/PublicInbox/View.pm
@@ -591,8 +591,9 @@ sub add_text_body { # callback for each_part
 		$diff = 1;
 		delete $ctx->{-long_path};
 		my $spfx;
-		if ($ibx->{-repo_objs}) {
-			if (index($upfx, '//') >= 0) { # absolute URL (Atom feeds)
+		# absolute URL (Atom feeds)
+		if ($ibx->{coderepo}) {
+			if (index($upfx, '//') >= 0) {
 				$spfx = $upfx;
 				$spfx =~ s!/([^/]*)/\z!/!;
 			} else {

lib/PublicInbox/WWW.pm
@@ -309,7 +309,7 @@ sub get_text {
 sub get_vcs_object ($$$;$) {
 	my ($ctx, $inbox, $oid, $filename) = @_;
 	my $r404 = invalid_inbox($ctx, $inbox);
-	return $r404 if $r404;
+	return $r404 if $r404 || !$ctx->{www}->{pi_cfg}->repo_objs($ctx->{ibx});
 	require PublicInbox::ViewVCS;
 	PublicInbox::ViewVCS::show($ctx, $oid, $filename);
 }

lib/PublicInbox/WwwStream.pm
@@ -84,7 +84,7 @@ sub coderepos ($) {
 	my $upfx = ($ctx->{-upfx} // ''). '../';
 	my @ret;
 	for my $cr_name (@$cr) {
-		my $urls = $cfg->{"coderepo.$cr_name.cgiturl"} // next;
+		my $urls = $cfg->get_all("coderepo.$cr_name.cgiturl") // next;
 		$ret[0] //= <<EOF;
 code repositories for the project(s) associated with this inbox:
 EOF

lib/PublicInbox/WwwText.pm
@@ -191,7 +191,7 @@ EOF
 EOF
 		my $pi_cfg = $ctx->{www}->{pi_cfg};
 		for my $cr_name (@$cr) {
-			my $urls = $pi_cfg->{"coderepo.$cr_name.cgiturl"};
+			my $urls = $pi_cfg->get_all("coderepo.$cr_name.cgiturl");
 			my $path = "/path/to/$cr_name";
 			$cr_name = dq_escape($cr_name);
 

t/config.t
@@ -212,7 +212,7 @@ EOF
 	my $cfg = PublicInbox::Config->new(\$str);
 	my $t1 = $cfg->lookup_name('test1');
 	my $t2 = $cfg->lookup_name('test2');
-	is($t1->{-repo_objs}->[0], $t2->{-repo_objs}->[0],
+	is($cfg->repo_objs($t1)->[0], $cfg->repo_objs($t2)->[0],
 		'inboxes share ::Git object');
 }
 

commit 6890430df808488a42184844bb74331644772b09

cgit: fix fallout from lazy coderepo loading
We can't completely instantiate our cgit wrapper without knowing
knowing cgit locations for serving static content.

Fixes: a5968dc059f655a ("config: lazy-load coderepos, support extindex")

lib/PublicInbox/Cgit.pm
@@ -53,14 +53,18 @@ sub locate_cgit ($) {
 sub new {
 	my ($class, $pi_cfg) = @_;
 	my ($cgit_bin, $cgit_data) = locate_cgit($pi_cfg);
-
+	# TODO: support gitweb and other repository viewers?
+	if (defined(my $cgitrc = $pi_cfg->{-cgitrc_unparsed})) {
+		$pi_cfg->parse_cgitrc($cgitrc, 0);
+	}
 	my $self = bless {
 		cmd => [ $cgit_bin ],
 		cgit_data => $cgit_data,
 		pi_cfg => $pi_cfg,
 	}, $class;
 
-	$pi_cfg->fill_all; # fill in -code_repos mapped to inboxes
+	# fill in -code_repos mapped to inboxes
+	$pi_cfg->each_inbox($pi_cfg->can('repo_objs'));
 
 	# some cgit repos may not be mapped to inboxes, so ensure those exist:
 	my $code_repos = $pi_cfg->{-code_repos};

lib/PublicInbox/Config.pm
@@ -323,10 +323,6 @@ sub _fill_code_repo {
 	my ($self, $nick) = @_;
 	my $pfx = "coderepo.$nick";
 
-	# TODO: support gitweb and other repository viewers?
-	if (defined(my $cgitrc = delete $self->{-cgitrc_unparsed})) {
-		parse_cgitrc($self, $cgitrc, 0);
-	}
 	my $dir = $self->{"$pfx.dir"}; # aka "GIT_DIR"
 	unless (defined $dir) {
 		warn "$pfx.dir unset\n";