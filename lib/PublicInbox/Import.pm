nojb/public-inbox

commit 7c2f36de2fb49dd756f9a8a1a44dbf7fb4b25f38

import: respect init.defaultBranch
This matches git v2.28.0+ behavior in case users prefer
a different name.

lib/PublicInbox/Import.pm
@@ -19,13 +19,21 @@ use PublicInbox::MDA;
 use PublicInbox::Eml;
 use POSIX qw(strftime);
 
+sub default_branch () {
+	state $default_branch = do {
+		my $r = popen_rd([qw(git config --global init.defaultBranch)]);
+		chomp(my $h = <$r> // '');
+		$h eq '' ? 'refs/heads/master' : $h;
+	}
+}
+
 sub new {
 	# we can't change arg order, this is documented in POD
 	# and external projects may rely on it:
 	my ($class, $git, $name, $email, $ibx) = @_;
-	my $ref = 'refs/heads/master';
+	my $ref;
 	if ($ibx) {
-		$ref = $ibx->{ref_head} // 'refs/heads/master';
+		$ref = $ibx->{ref_head};
 		$name //= $ibx->{name};
 		$email //= $ibx->{-primary_address};
 		$git //= $ibx->git;
@@ -34,7 +42,7 @@ sub new {
 		git => $git,
 		ident => "$name <$email>",
 		mark => 1,
-		ref => $ref,
+		ref => $ref // default_branch,
 		ibx => $ibx,
 		path_type => '2/38', # or 'v2'
 		lock_path => "$git->{git_dir}/ssoma.lock", # v2 changes this
@@ -441,7 +449,7 @@ sub run_die ($;$$) {
 	$? == 0 or die join(' ', @$cmd) . " failed: $?\n";
 }
 
-my @INIT_FILES = ('HEAD' => "ref: refs/heads/master\n",
+my @INIT_FILES = ('HEAD' => undef, # filled in at runtime
 		'description' => <<EOD,
 Unnamed repository; edit this file 'description' to name the repository.
 EOD
@@ -459,6 +467,7 @@ sub init_bare {
 	$dir = $dir->{git}->{git_dir} if ref($dir);
 	require File::Path;
 	File::Path::mkpath([ map { "$dir/$_" } qw(objects/info refs/heads) ]);
+	$INIT_FILES[1] //= 'ref: '.default_branch."\n";
 	for (my $i = 0; $i < @INIT_FILES; $i++) {
 		my $f = $dir.'/'.$INIT_FILES[$i++];
 		next if -f $f;

commit ea4e9025dd14f251996baf724e04fc478375b6a2

import: convert init.defaultBranch to fully qualified ref
init.defaultBranch expects a branch name, not a fully qualified ref.
git-init prepends "refs/heads/" automatically and unconditionally.

PublicInbox::Import::default_branch, however, incorrectly passes on
the init.defaultBranch value as is, leading to it being used in spots
where a fully qualified ref is required.  For example, with an
init.defaultBranch value of "master", public-inbox-index for a v2
repository would lead to an all.git repository where HEAD's content is
"ref: master" instead of "ref: refs/heads/master".

Prepend "refs/heads/" to the incoming init.defaultBranch value.

Fixes: 7c2f36de2fb49dd7 (import: respect init.defaultBranch)

lib/PublicInbox/Import.pm
@@ -25,7 +25,7 @@ sub default_branch () {
 				 { GIT_CONFIG => undef });
 		chomp(my $h = <$r> // '');
 		close $r;
-		$h eq '' ? 'refs/heads/master' : $h;
+		$h eq '' ? 'refs/heads/master' : "refs/heads/$h";
 	}
 }
 