nojb/public-inbox

commit 520be116e8a686cb223b48fad1de29201dee45be

www_listing: start updating for pagination + search
When dealing with thousands of inboxes, displaying all of
them on a single page isn't going to work.  So steal some
pagination and search results code from the message search
to generate some basic HTML output that looks good in w3m.

lib/PublicInbox/ManifestJsGz.pm
@@ -81,7 +81,7 @@ sub ibx_entry {
 	warn "E: $@" if $@;
 }
 
-sub hide_key { 'manifest' }
+sub hide_key { 'manifest' } # for WwwListing->list_match_i
 
 # overrides WwwListing->psgi_triple
 sub psgi_triple {

lib/PublicInbox/WwwListing.pm
@@ -5,7 +5,7 @@
 # Used by PublicInbox::WWW
 package PublicInbox::WwwListing;
 use strict;
-use PublicInbox::Hval qw(prurl fmt_ts);
+use PublicInbox::Hval qw(prurl fmt_ts ascii_html);
 use PublicInbox::Linkify;
 use PublicInbox::GzipFilter qw(gzf_maybe);
 use PublicInbox::ConfigIter;
@@ -13,18 +13,19 @@ use PublicInbox::WwwStream;
 use bytes (); # bytes::length
 
 sub ibx_entry {
-	my ($ctx, $ibx) = @_;
-	my $mtime = $ibx->modified;
-	my $ts = fmt_ts($mtime);
+	my ($ctx, $ibx, $ce) = @_;
+	$ce->{description} //= $ibx->description;
+	my $ts = fmt_ts($ce->{-modified} //= $ibx->modified);
 	my $url = prurl($ctx->{env}, $ibx->{url});
 	my $tmp = <<"";
 * $ts - $url
-  ${\$ibx->description}
+  $ce->{description}
 
 	if (defined(my $info_url = $ibx->{infourl})) {
 		$tmp .= '  ' . prurl($ctx->{env}, $info_url) . "\n";
 	}
-	push @{$ctx->{-list}}, [ $mtime, $tmp ];
+	push(@{$ctx->{-list}}, (scalar(@_) == 3 ? # $misc in use, already sorted
+				$tmp : [ $ce->{-modified}, $tmp ] ));
 }
 
 sub list_match_i { # ConfigIter callback
@@ -41,7 +42,7 @@ sub list_match_i { # ConfigIter callback
 	}
 }
 
-sub url_regexp {
+sub url_filter {
 	my ($ctx, $key, $default) = @_;
 	$key //= 'publicInbox.wwwListing';
 	$default //= '404';
@@ -50,9 +51,9 @@ again:
 	if ($v eq 'match=domain') {
 		my $h = $ctx->{env}->{HTTP_HOST} // $ctx->{env}->{SERVER_NAME};
 		$h =~ s/:[0-9]+\z//;
-		qr!\A(?:https?:)?//\Q$h\E(?::[0-9]+)?/!i;
+		(qr!\A(?:https?:)?//\Q$h\E(?::[0-9]+)?/!i, "url:$h");
 	} elsif ($v eq 'all') {
-		qr/./;
+		(qr/./, undef);
 	} elsif ($v eq '404') {
 		undef;
 	} else {
@@ -67,20 +68,120 @@ EOF
 
 sub hide_key { 'www' }
 
+sub add_misc_ibx { # MiscSearch->retry_reopen callback
+	my ($misc, $ctx, $re, $qs) = @_;
+	require PublicInbox::SearchQuery;
+	my $q = $ctx->{-sq} = PublicInbox::SearchQuery->new($ctx->{qp});
+	my $o = $q->{o};
+	my ($asc, $min, $max);
+	if ($o < 0) {
+		$asc = 1;
+		$o = -($o + 1); # so [-1] is the last element, like Perl lists
+	}
+	my $r = $q->{r};
+	my $opt = {
+		offset => $o,
+		asc => $asc,
+		relevance => $r,
+		limit => $q->{l}
+	};
+	$qs .= ' type:inbox';
+	if (my $user_query = $q->{'q'}) {
+		$qs = "( $qs ) AND ( $user_query )";
+	}
+	my $mset = $misc->mset($qs, $opt); # sorts by $MODIFIED (mtime)
+	$ctx->{-list} = [];
+	my $pi_cfg = $ctx->{www}->{pi_cfg};
+	for my $mi ($mset->items) {
+		my $doc = $mi->get_document;
+		my ($eidx_key) = PublicInbox::Search::xap_terms('Q', $doc);
+		$eidx_key // next;
+		my $ibx = $pi_cfg->lookup_eidx_key($eidx_key) // next;
+		next if $ibx->{-hide}->{$ctx->hide_key};
+		grep(/$re/, @{$ibx->{url}}) or next;
+		$ctx->ibx_entry($ibx, $misc->doc2ibx_cache_ent($doc));
+		if ($r) { # for descriptions in search_nav_bot
+			my $pct = PublicInbox::Search::get_pct($mi);
+			# only when sorting by relevance, ->items is always
+			# ordered descending:
+			$max //= $pct;
+			$min = $pct;
+		}
+	}
+	if ($r) { # for descriptions in search_nav_bot
+		$q->{-min_pct} = $min;
+		$q->{-max_pct} = $max;
+	}
+	$ctx->{-mset} = $mset;
+	psgi_triple($ctx);
+}
+
 sub response {
 	my ($class, $ctx) = @_;
 	bless $ctx, $class;
-	if (my $ALL = $ctx->{www}->{pi_cfg}->ALL) {
-		$ALL->misc->reopen;
-	}
-	my $re = $ctx->url_regexp or return $ctx->psgi_triple;
-	my $iter = PublicInbox::ConfigIter->new($ctx->{www}->{pi_cfg},
+	my ($re, $qs) = $ctx->url_filter;
+	$re // return $ctx->psgi_triple;
+	if (my $ALL = $ctx->{www}->{pi_cfg}->ALL) { # fast path
+		$ALL->misc->reopen->retry_reopen(\&add_misc_ibx,
+						$ctx, $re, $qs);
+	} else { # slow path, no [extindex "all"] configured
+		my $iter = PublicInbox::ConfigIter->new($ctx->{www}->{pi_cfg},
 						\&list_match_i, $re, $ctx);
-	sub {
-		$ctx->{-wcb} = $_[0]; # HTTP server callback
-		$ctx->{env}->{'pi-httpd.async'} ?
-				$iter->event_step : $iter->each_section;
+		sub {
+			$ctx->{-wcb} = $_[0]; # HTTP server callback
+			$ctx->{env}->{'pi-httpd.async'} ?
+					$iter->event_step : $iter->each_section;
+		}
 	}
+}
+
+sub mset_footer ($$) {
+	my ($ctx, $mset) = @_;
+	# no footer if too few matches
+	return '' if $mset->get_matches_estimated == $mset->size;
+	require PublicInbox::SearchView;
+	PublicInbox::SearchView::search_nav_bot($mset, $ctx->{-sq});
+}
+
+sub mset_nav_top {
+	my ($ctx, $mset) = @_;
+	my $q = $ctx->{-sq};
+	my $qh = $q->{'q'} // '';
+	utf8::decode($qh);
+	$qh = ascii_html($qh);
+	$qh = qq[\nvalue="$qh"] if $qh ne '';
+	my $rv = <<EOM;
+<form
+action="./"><pre><input
+name=q
+type=text$qh /><input
+type=submit
+value="locate inbox" /></pre></form><pre>
+EOM
+	chomp $rv;
+	if (defined($q->{'q'})) {
+		my $initial_q = $ctx->{-uxs_retried};
+		if (defined $initial_q) {
+			my $rewritten = $q->{'q'};
+			utf8::decode($initial_q);
+			utf8::decode($rewritten);
+			$initial_q = ascii_html($initial_q);
+			$rewritten = ascii_html($rewritten);
+			$rv .= " Warning: Initial query:\n <b>$initial_q</b>\n";
+			$rv .= " returned no results, used:\n";
+			$rv .= " <b>$rewritten</b>\n instead\n\n";
+		}
+		$rv .= 'Search results ordered by [';
+		if ($q->{r}) {
+			my $d = $q->qs_html(r => 0);
+			$rv .= qq{<a\nhref="?$d">updated</a>|<b>relevance</b>};
+		} else {
+			my $d = $q->qs_html(r => 1);
+			$rv .= qq{<b>updated</b>|<a\nhref="?$d">relevance</a>};
+		}
+		$rv .= ']';
+	}
+	$rv .= qq{</pre>};
 }
 
 sub psgi_triple {
@@ -90,17 +191,23 @@ sub psgi_triple {
 	my $gzf = gzf_maybe($h, $ctx->{env});
 	$gzf->zmore('<html><head><title>' .
 				'public-inbox listing</title>' .
-				'</head><body><pre>');
+				'</head><body>');
 	my $code = 404;
-	if (my $list = $ctx->{-list}) {
+	if (my $list = delete $ctx->{-list}) {
+		my $mset = delete $ctx->{-mset};
 		$code = 200;
-		# sort by ->modified
-		@$list = map { $_->[1] } sort { $b->[0] <=> $a->[0] } @$list;
+		if ($mset) { # already sorted, so search bar:
+			$gzf->zmore(mset_nav_top($ctx, $mset));
+		} else { # sort config dump by ->modified
+			@$list = map { $_->[1] }
+				sort { $b->[0] <=> $a->[0] } @$list;
+		}
 		$list = join("\n", @$list);
 		my $l = PublicInbox::Linkify->new;
-		$gzf->zmore($l->to_html($list));
+		$gzf->zmore('<pre>'.$l->to_html($list));
+		$gzf->zmore(mset_footer($ctx, $mset)) if $mset;
 	} else {
-		$gzf->zmore('no inboxes, yet');
+		$gzf->zmore('<pre>no inboxes, yet');
 	}
 	my $out = $gzf->zflush('</pre><hr><pre>'.
 			PublicInbox::WwwStream::code_footer($ctx->{env}) .

lib/PublicInbox/Config.pm
@@ -97,6 +97,11 @@ sub lookup_ei {
 	$self->{-ei_by_name}->{$name} //= _fill_ei($self, $name);
 }
 
+sub lookup_eidx_key {
+	my ($self, $eidx_key) = @_;
+	_lookup_fill($self, '-by_eidx_key', $eidx_key);
+}
+
 # special case for [extindex "all"]
 sub ALL { lookup_ei($_[0], 'all') }
 

lib/PublicInbox/MiscSearch.pm
@@ -59,7 +59,7 @@ sub misc_enquire_once { # retry_reopen callback
 	$eq->set_query($qr);
         my $desc = !$opt->{asc};
 	my $rel = $opt->{relevance} // 0;
-	if ($rel == -1) { # ORDER BY docid/UID
+	if ($rel == -1) { # ORDER BY docid
 		$eq->set_docid_order($PublicInbox::Search::ENQ_ASCENDING);
 		$eq->set_weighting_scheme($PublicInbox::Search::X{BoolWeight}->new);
 	} elsif ($rel) {
@@ -132,6 +132,23 @@ sub ibx_data_once {
 	}
 }
 
+sub doc2ibx_cache_ent { # @_ == ($self, $doc) OR ($doc)
+	my ($doc) = $_[-1];
+	my $d;
+	my $data = $json->decode($doc->get_data);
+	for (values %$data) {
+		$d = $_->{description} // next;
+		$d =~ s/ \[epoch [0-9]+\]\z// or next;
+		last;
+	}
+	{
+		uidvalidity => int_val($doc, $UIDVALIDITY),
+		-modified => int_val($doc, $MODIFIED),
+		# extract description from manifest.js.gz epoch description
+		description => $d
+	};
+}
+
 sub inbox_data {
 	my ($self, $ibx) = @_;
 	retry_reopen($self, \&ibx_data_once, $ibx);
@@ -141,20 +158,7 @@ sub ibx_cache_load {
 	my ($doc, $cache) = @_;
 	my ($eidx_key) = xap_terms('Q', $doc);
 	return unless defined($eidx_key); # expired
-	my $ce = $cache->{$eidx_key} = {};
-	$ce->{uidvalidity} = int_val($doc, $UIDVALIDITY);
-	$ce->{-modified} = int_val($doc, $MODIFIED);
-	$ce->{description} = do {
-		# extract description from manifest.js.gz epoch description
-		my $d;
-		my $data = $json->decode($doc->get_data);
-		for (values %$data) {
-			$d = $_->{description} // next;
-			$d =~ s/ \[epoch [0-9]+\]\z// or next;
-			last;
-		}
-		$d;
-	}
+	$cache->{$eidx_key} = doc2ibx_cache_ent($doc);
 }
 
 sub _nntpd_cache_load { # retry_reopen callback

lib/PublicInbox/SearchQuery.pm
@@ -1,7 +1,7 @@
 # Copyright (C) 2015-2021 all contributors <meta@public-inbox.org>
 # License: AGPL-3.0+ <https://www.gnu.org/licenses/agpl-3.0.txt>
 
-# used by PublicInbox::SearchView
+# used by PublicInbox::SearchView and PublicInbox::WwwListing
 package PublicInbox::SearchQuery;
 use strict;
 use v5.10.1;
@@ -32,11 +32,12 @@ sub qs_html {
 	if (scalar(keys(%override))) {
 		$self = bless { (%$self, %override) }, ref($self);
 	}
-
-	my $q = uri_escape($self->{'q'}, MID_ESC);
-	$q =~ s/%20/+/g; # improve URL readability
-	my $qs = "q=$q";
-
+	my $qs = '';
+	if (defined(my $q = $self->{'q'})) {
+		$q = uri_escape($q, MID_ESC);
+		$q =~ s/%20/+/g; # improve URL readability
+		$qs .= "q=$q";
+	}
 	if (my $o = $self->{o}) { # ignore o == 0
 		$qs .= "&amp;o=$o";
 	}

lib/PublicInbox/SearchView.pm
@@ -219,7 +219,7 @@ sub search_nav_top {
 	$rv .= qq{</pre></form><pre>};
 }
 
-sub search_nav_bot {
+sub search_nav_bot { # also used by WwwListing for searching extindex miscidx
 	my ($mset, $q) = @_;
 	my $total = $mset->get_matches_estimated;
 	my $l = $q->{l};

commit e3a7014b7594e918901d803d9d966af70e8900e0

www: fix manifest.js.gz for default publicInbox.grokManifest
ManifestJsGz->response was not invoking the new "url_filter"
method properly.  Furthermore, fix url_filter for returning 404
responses.

Reported-by: Kyle Meyer <kyle@kyleam.com>
Link: https://public-inbox.org/meta/87fsx3128a.fsf@kyleam.com/
Fixes: 520be116e8a686cb ("www_listing: start updating for pagination + search")

lib/PublicInbox/ManifestJsGz.pm
@@ -13,11 +13,11 @@ use HTTP::Date qw(time2str);
 
 my $json = PublicInbox::Config::json();
 
-sub url_regexp {
+sub url_filter {
 	my ($ctx) = @_;
 	# grokmirror uses relative paths, so it's domain-dependent
 	# SUPER calls PublicInbox::WwwListing::url_filter
-	($ctx->url_filter('publicInbox.grokManifest', 'match=domain'))[0];
+	$ctx->SUPER::url_filter('publicInbox.grokManifest', 'match=domain');
 }
 
 sub inject_entry ($$$;$) {

lib/PublicInbox/WwwListing.pm
@@ -55,7 +55,7 @@ again:
 	} elsif ($v eq 'all') {
 		(qr/./, undef);
 	} elsif ($v eq '404') {
-		undef;
+		(undef, undef);
 	} else {
 		warn <<EOF;
 `$v' is not a valid value for `$key'

t/www_listing.t
@@ -1,14 +1,15 @@
+#!perl -w
 # Copyright (C) 2019-2021 all contributors <meta@public-inbox.org>
 # License: AGPL-3.0+ <https://www.gnu.org/licenses/agpl-3.0.txt>
 # manifest.js.gz generation and grok-pull integration test
 use strict;
-use warnings;
+use v5.10.1;
 use Test::More;
 use PublicInbox::Spawn qw(which);
 use PublicInbox::TestCommon;
 use PublicInbox::Import;
-require_mods(qw(json URI::Escape Plack::Builder Digest::SHA
-		IO::Compress::Gzip IO::Uncompress::Gunzip HTTP::Tiny));
+use IO::Uncompress::Gunzip qw(gunzip);
+require_mods(qw(json URI::Escape Plack::Builder Digest::SHA HTTP::Tiny));
 require PublicInbox::WwwListing;
 require PublicInbox::ManifestJsGz;
 use PublicInbox::Config;
@@ -32,21 +33,23 @@ like($bare->manifest_entry->{fingerprint}, qr/\A[a-f0-9]{40}\z/,
 	'got fingerprint with non-empty repo');
 
 sub tiny_test {
-	my ($json, $host, $port) = @_;
-	my $tmp;
+	my ($json, $host, $port, $html) = @_;
+	my ($tmp, $res);
 	my $http = HTTP::Tiny->new;
-	my $res = $http->get("http://$host:$port/");
-	is($res->{status}, 200, 'got HTML listing');
-	like($res->{content}, qr!</html>!si, 'listing looks like HTML');
+	if ($html) {
+		$res = $http->get("http://$host:$port/");
+		is($res->{status}, 200, 'got HTML listing');
+		like($res->{content}, qr!</html>!si, 'listing looks like HTML');
 
-	$res = $http->get("http://$host:$port/", {'Accept-Encoding'=>'gzip'});
-	is($res->{status}, 200, 'got gzipped HTML listing');
-	IO::Uncompress::Gunzip::gunzip(\(delete $res->{content}) => \$tmp);
-	like($tmp, qr!</html>!si, 'unzipped listing looks like HTML');
-
+		$res = $http->get("http://$host:$port/",
+				{'Accept-Encoding'=>'gzip'});
+		is($res->{status}, 200, 'got gzipped HTML listing');
+		gunzip(\(delete $res->{content}) => \$tmp);
+		like($tmp, qr!</html>!si, 'unzipped listing looks like HTML');
+	}
 	$res = $http->get("http://$host:$port/manifest.js.gz");
 	is($res->{status}, 200, 'got manifest');
-	IO::Uncompress::Gunzip::gunzip(\(delete $res->{content}) => \$tmp);
+	gunzip(\(delete $res->{content}) => \$tmp);
 	unlike($tmp, qr/"modified":\s*"/, 'modified is an integer');
 	my $manifest = $json->decode($tmp);
 	ok(my $clone = $manifest->{'/alt'}, '/alt in manifest');
@@ -95,10 +98,9 @@ SKIP: {
 		"lorelei \xc4\x80"), 0,
 		'set gitweb user');
 	ok(unlink("$bare->{git_dir}/description"), 'removed bare/description');
-	open $fh, '>', $cfgfile or die;
-	print $fh <<"" or die;
-[publicinbox]
-	wwwlisting = all
+	open $fh, '>', $cfgfile or xbail "open $cfgfile: $!";
+	$fh->autoflush(1);
+	print $fh <<"" or xbail "print $!";
 [publicinbox "bare"]
 	inboxdir = $bare->{git_dir}
 	url = http://$host/bare
@@ -112,13 +114,22 @@ SKIP: {
 	url = http://$host/v2
 	address = v2\@example.com
 
-	close $fh or die;
 	my $env = { PI_CONFIG => $cfgfile };
 	my $cmd = [ '-httpd', '-W0', "--stdout=$out", "--stderr=$err" ];
 	$td = start_script($cmd, $env, { 3 => $sock });
-	$sock = undef;
 
+	# default publicinboxGrokManifest match=domain default
 	tiny_test($json, $host, $port);
+	undef $td;
+
+	print $fh <<"" or xbail "print $!";
+[publicinbox]
+	wwwlisting = all
+
+	close $fh or xbail "close $!";
+	$td = start_script($cmd, $env, { 3 => $sock });
+	tiny_test($json, $host, $port, 1);
+	undef $sock;
 
 	my $grok_pull = which('grok-pull') or
 		skip('grok-pull not available', 12);