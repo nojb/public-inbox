nojb/public-inbox

commit 176cd51f9aa81b741deb00cd8096428eb6712b9e

daemon: quiet down Eml-related warnings
Email::Address::XS is quite noisy and there's nothing we can
really do about messages we're serving from read-only daemons.

lib/PublicInbox/Daemon.pm
@@ -20,6 +20,7 @@ use PublicInbox::EOFpipe;
 use PublicInbox::Sigfd;
 use PublicInbox::Git;
 use PublicInbox::GitAsyncCat;
+use PublicInbox::Eml;
 our $SO_ACCEPTFILTER = 0x1000;
 my @CMD;
 my ($set_user, $oldset);
@@ -642,6 +643,7 @@ sub run ($$$;$) {
 	# localize GCF2C for tests:
 	local $PublicInbox::GitAsyncCat::GCF2C;
 	local $PublicInbox::Git::async_warn = 1;
+	local $SIG{__WARN__} = \&PublicInbox::Eml::warn_ignore_cb;
 
 	daemon_loop($refresh, $post_accept, $tlsd, $af_default);
 	PublicInbox::DS->Reset;

commit 47035fd4b54ceb0da2f71d28b99a962acac576f4

daemon: set $SIG{__WARN__} properly
Eml->warn_ignore_cb itself returns a callback, so creating a
reference to it was wrong when assigning it to $SIG{__WARN__};

Fixes: 176cd51f9aa81b74 ("daemon: quiet down Eml-related warnings")

lib/PublicInbox/Daemon.pm
@@ -643,7 +643,7 @@ sub run ($$$;$) {
 	# localize GCF2C for tests:
 	local $PublicInbox::GitAsyncCat::GCF2C;
 	local $PublicInbox::Git::async_warn = 1;
-	local $SIG{__WARN__} = \&PublicInbox::Eml::warn_ignore_cb;
+	local $SIG{__WARN__} = PublicInbox::Eml::warn_ignore_cb();
 
 	daemon_loop($refresh, $post_accept, $tlsd, $af_default);
 	PublicInbox::DS->Reset;