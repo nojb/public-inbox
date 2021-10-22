nojb/public-inbox

commit c447bbbddb4ac8e1e9b7c35a15c09776d18501db

wwwatomstream: simplify feed_update callers
We always return Z (UTC) times, anyways, so we'll always
use gmtime() on the seconds-after-the-epoch.

lib/PublicInbox/WwwAtomStream.pm
@@ -103,7 +103,7 @@ sub atom_header {
 		qq(\nhref="$base_url"/>) .
 	qq(<link\nrel="self"\nhref="$self_url"/>) .
 	qq(<id>$page_id</id>) .
-	feed_updated(gmtime($mtime));
+	feed_updated($mtime);
 }
 
 # returns undef or string
@@ -125,9 +125,7 @@ sub feed_entry {
 		$irt = '';
 	}
 	my $href = $base . mid_href($mid) . '/';
-	my $t = msg_timestamp($hdr);
-	my @t = gmtime(defined $t ? $t : time);
-	my $updated = feed_updated(@t);
+	my $updated = feed_updated(msg_timestamp($hdr));
 
 	my $title = $hdr->header('Subject');
 	$title = '(no subject)' unless defined $title && $title ne '';
@@ -158,7 +156,7 @@ sub feed_entry {
 }
 
 sub feed_updated {
-	'<updated>' . strftime('%Y-%m-%dT%H:%M:%SZ', @_) . '</updated>';
+	'<updated>' . strftime('%Y-%m-%dT%H:%M:%SZ', gmtime(@_)) . '</updated>';
 }
 
 1;

commit 45f87867107724371d80a563099ab93eda1d919b

wwwatomstream: call gmtime with scalar
When the gmtime() calls were moved from feed_entry() and atom_header()
into feed_updated() in c447bbbd, @_ rather than a scalar was passed to
gmtime().  As a result, feed <updated> values end up as
"1970-01-01T00:00:00Z".

Switch back to using a scalar argument to restore the correct
timestamps.

Fixes: c447bbbddb4ac8e1 ("wwwatomstream: simplify feed_update callers")

lib/PublicInbox/WwwAtomStream.pm
@@ -161,7 +161,8 @@ sub feed_entry {
 }
 
 sub feed_updated {
-	'<updated>' . strftime('%Y-%m-%dT%H:%M:%SZ', gmtime(@_)) . '</updated>';
+	my ($t) = @_;
+	'<updated>' . strftime('%Y-%m-%dT%H:%M:%SZ', gmtime($t)) . '</updated>';
 }
 
 1;

t/psgi_v2.t
@@ -92,6 +92,11 @@ my $client0 = sub {
 	@bodies = ($res->content =~ /^(hello [^<]+)$/mg);
 	is_deeply(\@bodies, [ "hello world!\n", "hello world\n" ],
 		'new.html ordering is chronological');
+
+	$res = $cb->(GET('/v2test/new.atom'));
+	my @dates = ($res->content =~ m!title><updated>([^<]+)</updated>!g);
+	is_deeply(\@dates, [ "1993-10-02T00:01:00Z", "1993-10-02T00:00:00Z" ],
+		'Date headers made it through');
 };
 test_psgi(sub { $www->call(@_) }, $client0);
 my $env = { TMPDIR => $tmpdir, PI_CONFIG => $cfgpath };