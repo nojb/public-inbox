nojb/public-inbox

commit 1f887bd51d92b0d4fea4d8e3c36f4328c6b0ae76

lei inspect: add atfork hook
This is necessary for in case an inspect command is run
in a parallel with other commands.

lib/PublicInbox/LeiInspect.pm
@@ -289,4 +289,10 @@ sub _complete_inspect {
 	# TODO: message-ids?, blobs? could get expensive...
 }
 
+sub input_only_atfork_child {
+	my ($self) = @_;
+	$self->{lei}->_lei_atfork_child;
+	$self->SUPER::ipc_atfork_child;
+}
+
 1;

commit b204d687ba262c2374fbf0f972bb1f3a824b2c00

lei inspect: fix atfork hook
The misnamed sub wasn't firing, but was unlikely to be
noticeable given the short lifetime of the process.

Fixes: 1f887bd51d92b0d4 ("lei inspect: add atfork hook")

lib/PublicInbox/LeiInspect.pm
@@ -294,7 +294,7 @@ sub _complete_inspect {
 	# TODO: message-ids?, blobs? could get expensive...
 }
 
-sub input_only_atfork_child {
+sub ipc_atfork_child {
 	my ($self) = @_;
 	$self->{lei}->_lei_atfork_child;
 	$self->SUPER::ipc_atfork_child;