nojb/public-inbox

commit a6d50dc1098c01a1c3092a7f0f8aa154331600a3

www: gzip_filter: gracefully handle socket ->write failures
Socket ->write failures are expected and common for TCP traffic,
especially if it's facing unreliable remote connections.  So
just bail out silently if our {gz} field was already clobbered
during the small bit of recursion we hit on ->write failures
from async responses.

This ought to fix some GzipFilter::zflush errors (via $forward
->close from PublicInbox::HTTP) I've been noticing on
deployments running -netd.  I'm still unsure as to why I hadn't
seen them before, but it might've only been ignorance on my
part...

Link: https://public-inbox.org/meta/20220802065436.GA13935@dcvr/

lib/PublicInbox/GzipFilter.pm
@@ -149,10 +149,11 @@ sub zflush ($;$) {
 	my $zbuf = delete $self->{zbuf};
 	my $gz = delete $self->{gz};
 	my $err;
-	if (defined $_[1]) {
+	if (defined $_[1]) { # it's a bug iff $gz is undef w/ $_[1]
 		$err = $gz->deflate($_[1], $zbuf);
 		die "gzip->deflate: $err" if $err != Z_OK;
 	}
+	$gz // return; # not a bug, recursing on DS->write failure
 	$err = $gz->flush($zbuf);
 	die "gzip->flush: $err" if $err != Z_OK;
 	$zbuf;

commit 27876bd1269d18f697847a724a8465318cb3cd67

www: gzip_filter: avoid errors after ->write failure
->zflush must return a string to its caller, not undef.
Additionally, {http_out} may be deleted on ->write if ->close
recurses.

This should fix the following errors:
  Use of uninitialized value $_[1] in string eq at PublicInbox/HTTP.pm line 211.
  E: Can't call method "close" on an undefined value at GzipFilter.pm line 167.

Fixes: a6d50dc1098c01a1 (www: gzip_filter: gracefully handle socket ->write failures, 2022-08-03)

lib/PublicInbox/GzipFilter.pm
@@ -154,7 +154,7 @@ sub zflush ($;$) {
 		$err = $gz->deflate($_[1], $zbuf);
 		die "gzip->deflate: $err" if $err != Z_OK;
 	}
-	$gz // return; # not a bug, recursing on DS->write failure
+	$gz // return ''; # not a bug, recursing on DS->write failure
 	$err = $gz->flush($zbuf);
 	die "gzip->flush: $err" if $err != Z_OK;
 	$zbuf;
@@ -164,7 +164,7 @@ sub close {
 	my ($self) = @_;
 	my $http_out = http_out($self) // return;
 	$http_out->write(zflush($self));
-	delete($self->{http_out})->close;
+	(delete($self->{http_out}) // return)->close;
 }
 
 sub bail  {