nojb/public-inbox

commit 1c0ec857d041dc287919917676bb6678301085d6

gcf2: support worktree $GIT_DIR
We must use `git rev-parse --git-path objects' instead of
blindly appending '/objects' to $GIT_DIR, since appending
doesn't work when $GIT_DIR is a worktree.

lib/PublicInbox/Gcf2.pm
@@ -80,7 +80,8 @@ EOM
 }
 
 sub add_alt ($$) {
-	my ($gcf2, $objdir) = @_;
+	my ($gcf2, $git_dir) = @_;
+	my $objdir = PublicInbox::Git->new($git_dir)->git_path('objects');
 
 	# libgit2 (tested 0.27.7+dfsg.1-0.2 and 0.28.3+dfsg.1-1~bpo10+1
 	# in Debian) doesn't handle relative epochs properly when nested
@@ -120,7 +121,7 @@ sub loop (;$) {
 	while (<STDIN>) {
 		chomp;
 		my ($oid, $git_dir) = split(/ /, $_, 2);
-		$seen{$git_dir} //= add_alt($gcf2, "$git_dir/objects");
+		$seen{$git_dir} //= add_alt($gcf2, $git_dir);
 		if (!$gcf2->cat_oid(1, $oid)) {
 			# retry once if missing.  We only get unabbreviated OIDs
 			# from SQLite or Xapian DBs, here, so malicious clients
@@ -128,7 +129,7 @@ sub loop (;$) {
 			warn "I: $$ $oid missing, retrying in $git_dir\n";
 
 			$gcf2 = new();
-			%seen = ($git_dir => add_alt($gcf2,"$git_dir/objects"));
+			%seen = ($git_dir => add_alt($gcf2, $git_dir);
 			$check_at = clock_gettime(CLOCK_MONOTONIC) + $exp;
 
 			if ($gcf2->cat_oid(1, $oid)) {

commit 33f2cbeeca73c35dffcbfba53a614c0516f55cdc

gcf2: fix syntax error and require PublicInbox::Git
I failed to notice these since I uninstalled libgit2 for
benchmarking and kept it uninstalled since my git(1) install
is faster.

Fixes: 1c0ec857d041 "gcf2: support worktree $GIT_DIR"

lib/PublicInbox/Gcf2.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2020-2021 all contributors <meta@public-inbox.org>
+# Copyright (C) all contributors <meta@public-inbox.org>
 # License: AGPL-3.0+ <https://www.gnu.org/licenses/agpl-3.0.txt>
 
 # backend for a git-cat-file-workalike based on libgit2,
@@ -11,6 +11,8 @@ use Fcntl qw(LOCK_EX SEEK_SET);
 use Time::HiRes qw(clock_gettime CLOCK_MONOTONIC);
 use IO::Handle; # autoflush
 use File::Path qw(make_path);
+use PublicInbox::Git;
+
 BEGIN {
 	my (%CFG, $c_src);
 	# PublicInbox::Spawn will set PERL_INLINE_DIRECTORY
@@ -129,7 +131,7 @@ sub loop (;$) {
 			warn "I: $$ $oid missing, retrying in $git_dir\n";
 
 			$gcf2 = new();
-			%seen = ($git_dir => add_alt($gcf2, $git_dir);
+			%seen = ($git_dir => add_alt($gcf2, $git_dir));
 			$check_at = clock_gettime(CLOCK_MONOTONIC) + $exp;
 
 			if ($gcf2->cat_oid(1, $oid)) {