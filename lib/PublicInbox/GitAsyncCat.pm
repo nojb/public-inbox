nojb/public-inbox

commit f9ac22a4b485ae5f8ad981027d8608c13f09530e

git_async_cat: automatically cleanup temporary gits
This prevents temporary directories and git processes from
lingering around after WWW solver requests.

lib/PublicInbox/GitAsyncCat.pm
@@ -45,6 +45,12 @@ sub event_step {
 	}
 }
 
+sub git_tmp_cleanup {
+	my ($git) = @_;
+	$git->cleanup(1) and
+		PublicInbox::DS::add_timer(3, \&git_tmp_cleanup, $git);
+}
+
 sub ibx_async_cat ($$$$) {
 	my ($ibx, $oid, $cb, $arg) = @_;
 	my $git = $ibx->{git} // $ibx->git;
@@ -63,6 +69,8 @@ sub ibx_async_cat ($$$$) {
 		$git->{async_cat} //= do {
 			my $self = bless { git => $git }, __PACKAGE__;
 			$git->{in}->blocking(0);
+			$git->{-tmp} and PublicInbox::DS::add_uniq_timer(
+						3, \&git_tmp_cleanup, $git);
 			$self->SUPER::new($git->{in}, EPOLLIN|EPOLLET);
 			\undef; # this is a true ref()
 		};

commit ac22adc97342323e056bb36f9434458cc822e7d9

www_stream: use DESTROY to cleanup temporary gits
Relying on a timer to handle cleanup in f9ac22a4b485 was
sub-optimal since the delay could prove expensive under heavy
traffic.  So rely on ->DESTROY instead since we we no longer
hold reference cycles by the time the show_blob callback
executes.

Fixes: f9ac22a4b485 ("git_async_cat: automatically cleanup temporary gits")

lib/PublicInbox/GitAsyncCat.pm
@@ -45,12 +45,6 @@ sub event_step {
 	}
 }
 
-sub git_tmp_cleanup {
-	my ($git) = @_;
-	$git->cleanup(1) and
-		PublicInbox::DS::add_timer(3, \&git_tmp_cleanup, $git);
-}
-
 sub ibx_async_cat ($$$$) {
 	my ($ibx, $oid, $cb, $arg) = @_;
 	my $git = $ibx->{git} // $ibx->git;
@@ -69,8 +63,6 @@ sub ibx_async_cat ($$$$) {
 		$git->{async_cat} //= do {
 			my $self = bless { git => $git }, __PACKAGE__;
 			$git->{in}->blocking(0);
-			$git->{-tmp} and PublicInbox::DS::add_uniq_timer(
-						3, \&git_tmp_cleanup, $git);
 			$self->SUPER::new($git->{in}, EPOLLIN|EPOLLET);
 			\undef; # this is a true ref()
 		};

lib/PublicInbox/ViewVCS.pm
@@ -414,6 +414,7 @@ blob $oid $size bytes $raw_link</pre>
 EOM
 	}
 	@{$ctx->{-paths}} = ($path, $raw_link);
+	bless $ctx, 'PublicInbox::WwwStream'; # for DESTROY
 	$ctx->{git} = $git;
 	if ($ctx->{env}->{'pi-httpd.async'}) {
 		ibx_async_cat($ctx, $oid, \&show_blob, $ctx);

lib/PublicInbox/WwwStream.pm
@@ -220,4 +220,9 @@ sub html_init {
 	print { $ctx->zfh } html_top($ctx);
 }
 
+sub DESTROY {
+	my ($ctx) = @_;
+	$ctx->{git}->cleanup if $ctx->{git} && $ctx->{git}->{-tmp};
+}
+
 1;